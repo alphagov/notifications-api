d275ba83a23ff0ef89162138f238b9b930e93918
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-01-19 11:23:09 +0000

    Added endpoints for the proxy to notifications.
    - this uses alpha API for delivery
    - no DB model included as just proving
    - all notifications for same service at the moment (!)

--- app/__init__.py
+++ app/__init__.py
@@ -7,10 +7,12 @@ from flask_marshmallow import Marshmallow
 from werkzeug.local import LocalProxy
 from config import configs
 from utils import logging
+from notify_client import NotifyAPIClient
 
 
 db = SQLAlchemy()
 ma = Marshmallow()
+notify_alpha_client = NotifyAPIClient()
 
 api_user = LocalProxy(lambda: _request_ctx_stack.top.api_user)
 
@@ -25,17 +27,20 @@ def create_app(config_name):
     ma.init_app(application)
     init_app(application)
     logging.init_app(application)
+    notify_alpha_client.init_app(application)
 
     from app.service.rest import service as service_blueprint
     from app.user.rest import user as user_blueprint
     from app.template.rest import template as template_blueprint
     from app.status.healthcheck import status as status_blueprint
     from app.job.rest import job as job_blueprint
+    from app.notifications.rest import notifications as notifications_blueprint
 
     application.register_blueprint(service_blueprint, url_prefix='/service')
     application.register_blueprint(user_blueprint, url_prefix='/user')
     application.register_blueprint(template_blueprint, url_prefix="/template")
     application.register_blueprint(status_blueprint, url_prefix='/status')
+    application.register_blueprint(notifications_blueprint, url_prefix='/notifications')
     application.register_blueprint(job_blueprint)
 
     return application
