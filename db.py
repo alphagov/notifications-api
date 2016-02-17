5f4b6a7db93a7e201c3ec9f0bf63e5a8e96e8544
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2016-02-17 11:01:15 +0000

    Fix db.py script for credstash secrets

--- db.py
+++ db.py
@@ -2,8 +2,11 @@ from flask.ext.script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
 from credstash import getAllSecrets
+import os
 
 secrets = getAllSecrets(region="eu-west-1")
+for key, val in secrets.items():
+    os.environ[key] = val
 
 application = create_app()
 
