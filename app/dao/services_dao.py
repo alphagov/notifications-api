e4d315a5ff9881240bfca612fd537d06c0dfcf29
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-02-19 15:53:15 +0000

    Refactored the services dao to be a little cleaner
    - some things we don't need
    	- bulk update of users
    	- delete service
    
    Now returns None if can't find an object

--- app/dao/services_dao.py
+++ app/dao/services_dao.py
@@ -1,48 +1,44 @@
 import json
-from datetime import datetime
-from sqlalchemy.orm import load_only
 from . import DAOException
 from app import db
-from app.models import (Service, User)
+from app.models import Service
+from sqlalchemy import asc
 
 
-def save_model_service(service, update_dict=None):
-    users_list = update_dict.get('users', []) if update_dict else getattr(service, 'users', [])
-    if not users_list:
-        error_msg = {'users': ['Missing data for required attribute']}
-        raise DAOException(json.dumps(error_msg))
-    if update_dict:
-        # Make sure the update_dict doesn't contain conflicting
-        update_dict.pop('id', None)
-        update_dict.pop('users', None)
-        # TODO optimize this algorithm
-        for i, x in enumerate(service.users):
-            if x not in users_list:
-                service.users.remove(x)
-            else:
-                users_list.remove(x)
-        for x in users_list:
-            service.users.append(x)
-        Service.query.filter_by(id=service.id).update(update_dict)
-    else:
-        db.session.add(service)
+def dao_fetch_all_services():
+    return Service.query.order_by(asc(Service.created_at)).all()
+
+
+def dao_fetch_service_by_id(service_id):
+    return Service.query.filter_by(id=service_id).first()
+
+
+def dao_fetch_all_services_by_user(user_id):
+    return Service.query.filter(Service.users.any(id=user_id)).order_by(asc(Service.created_at)).all()
+
+
+def dao_fetch_service_by_id_and_user(service_id, user_id):
+    return Service.query.filter(Service.users.any(id=user_id)).filter_by(id=service_id).first()
+
+
+def dao_create_service(service, user):
+    service.users.append(user)
+    db.session.add(service)
     db.session.commit()
 
 
-def delete_model_service(service):
-    db.session.delete(service)
+def dao_update_service(service):
+    db.session.add(service)
     db.session.commit()
 
 
-def get_model_services(service_id=None, user_id=None, _raise=True):
-    # TODO need better mapping from function params to sql query.
-    if user_id and service_id:
-        return Service.query.filter(
-            Service.users.any(id=user_id)).filter_by(id=service_id).one()
-    elif service_id:
-        result = Service.query.filter_by(id=service_id).one() if _raise else Service.query.filter_by(
-            id=service_id).first()
-        return result
-    elif user_id:
-        return Service.query.filter(Service.users.any(id=user_id)).all()
-    return Service.query.all()
+def dao_add_user_to_service(service, user):
+    service.users.append(user)
+    db.session.add(service)
+    db.session.commit()
+
+
+def dao_remove_user_from_service(service, user):
+    service.users.remove(user)
+    db.session.add(service)
+    db.session.commit()
