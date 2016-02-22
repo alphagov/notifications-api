1ea94b2b98fb2ba517ef11acacf6e895a15e0466
Adam Shimali <adam.shimali@gmail.com>
2016-02-22 15:01:30 +0000

    Fixed clashing migration file number

--- migrations/versions/0021_add_job_metadata.py
+++ migrations/versions/0021_add_job_metadata.py
@@ -1,14 +1,14 @@
 """empty message
 
-Revision ID: 0020_add_job_metadata
-Revises: 0019_unique_servicename
+Revision ID: 0021_add_job_metadata
+Revises: 0020_email_has_subject
 Create Date: 2016-02-22 12:33:02.360780
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '0020_add_job_metadata'
-down_revision = '0019_unique_servicename'
+revision = '0021_add_job_metadata'
+down_revision = '0020_email_has_subject'
 
 from alembic import op
 import sqlalchemy as sa
