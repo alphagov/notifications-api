fc8e27e58238be8b8cb1d5f86bcaa11b5ff8aa2a
Adam Shimali <adam.shimali@gmail.com>
2016-03-03 15:17:14 +0000

    [WIP] When user is added to a service a list of permissions groups are used to assign the correct permissions to the user.
    Last slice will be to update invite status.

---
+++ app/permissions_utils.py
@@ -0,0 +1,27 @@
+from app.models import (
+    MANAGE_USERS,
+    MANAGE_TEMPLATES,
+    MANAGE_SETTINGS,
+    SEND_TEXTS,
+    SEND_EMAILS,
+    SEND_LETTERS,
+    MANAGE_API_KEYS,
+    ACCESS_DEVELOPER_DOCS
+)
+
+from app.schemas import permission_schema
+
+
+permissions_groups = {'send_messages': [SEND_TEXTS, SEND_EMAILS, SEND_LETTERS],
+                      'manage_service': [MANAGE_USERS, MANAGE_SETTINGS, MANAGE_TEMPLATES],
+                      'manage_api_keys': [MANAGE_API_KEYS, ACCESS_DEVELOPER_DOCS]}
+
+
+def get_permissions_by_group(permission_groups):
+    requested_permissions = []
+    for group in permission_groups:
+        permissions = permissions_groups[group]
+        for permission in permissions:
+            requested_permissions.append({'permission': permission})
+    permissions, errors = permission_schema.load(requested_permissions, many=True)
+    return permissions
