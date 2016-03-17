6aec6a0bdaddb4ee6c4bb69487c7f8e6bcb6ac31
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-03-17 12:51:14 +0000

    Update as per pull request comments:
    - context manager for file handling
    - os.environ.update for setting overrides

--- aws_run_celery.py
+++ aws_run_celery.py
@@ -8,13 +8,11 @@ default_env_file = '/home/ubuntu/environment'
 environment = 'live'
 
 if os.path.isfile(default_env_file):
-    environment_file = open(default_env_file, 'r')
-    environment = environment_file.readline().strip()
+    with open(default_env_file, 'r') as environment_file:
+        environment = environment_file.readline().strip()
 
 # on aws get secrets and export to env
-secrets = getAllSecrets(region="eu-west-1")
-for key, val in secrets.items():
-    os.environ[key] = val
+os.environ.update(getAllSecrets(region="eu-west-1"))
 
 os.environ['NOTIFY_API_ENVIRONMENT'] = configs[environment]
 
