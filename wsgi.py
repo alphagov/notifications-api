be0a69d4169a65cb166ee08296b88ee9bf09b2a6
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-03-17 11:47:44 +0000

    Allowing overrides on a per environment basis

--- wsgi.py
+++ wsgi.py
@@ -2,13 +2,24 @@ import os
 
 from app import create_app
 from credstash import getAllSecrets
+from config import configs
+
+
+default_env_file = '/home/ubuntu/environment'
+environment = 'live'
+
+if os.path.isfile(default_env_file):
+    environment_file = open(default_env_file, 'r')
+    environment = environment_file.readline().strip()
 
 # on aws get secrets and export to env
 secrets = getAllSecrets(region="eu-west-1")
 for key, val in secrets.items():
     os.environ[key] = val
 
+os.environ['NOTIFY_API_ENVIRONMENT'] = configs[environment]
+
 application = create_app()
 
 if __name__ == "__main__":
-        application.run()
+    application.run()
