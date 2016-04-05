b0a4f5cea2bb1c493fe269931fa4dbc8f3a25023
Nicholas Staples <nicholas.staples@solution-tribute.com>
2016-04-05 14:28:19 +0100

    Remove csv after process job is finished.
    Fixed new tests.

--- app/aws/s3.py
+++ app/aws/s3.py
@@ -1,7 +1,16 @@
 from boto3 import resource
 
 
-def get_job_from_s3(bucket_name, job_id):
+def get_s3_job_object(bucket_name, job_id):
     s3 = resource('s3')
-    key = s3.Object(bucket_name, '{}.csv'.format(job_id))
-    return key.get()['Body'].read().decode('utf-8')
+    return s3.Object(bucket_name, '{}.csv'.format(job_id))
+
+
+def get_job_from_s3(bucket_name, job_id):
+    obj = get_s3_job_object(bucket_name, job_id)
+    return obj.get()['Body'].read().decode('utf-8')
+
+
+def remove_job_from_s3(bucket_name, job_id):
+    obj = get_s3_job_object(bucket_name, job_id)
+    return obj.delete()
