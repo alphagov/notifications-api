ba874cfd43b08b05eda0b527dfe0543394bed1b1
Adam Shimali <adam.shimali@gmail.com>
2016-05-11 12:39:39 +0100

    Only trigger dependent build for master, staging and live

--- scripts/trigger-dependent-build.sh
+++ scripts/trigger-dependent-build.sh
@@ -7,6 +7,12 @@
 #     - ./trigger-dependent-build
 #
 
-echo "Triggering dependent build for $TRAVIS_BRANCH"
-
-curl -vvv -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $auth_token" -d '{"request":{"branch":"master","config":{"env":{"global":["ENVIRONMENT='$TRAVIS_BRANCH'"]}}}}' https://api.travis-ci.org/repo/alphagov%2Fnotifications-functional-tests/requests
+case $TRAVIS_BRANCH in
+  master|staging|live)
+    echo "Triggering dependent build for $TRAVIS_BRANCH"
+    curl -vvv -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $auth_token" -d '{"request":{"branch":"master","config":{"env":{"global":["ENVIRONMENT='$TRAVIS_BRANCH'"]}}}}' https://api.travis-ci.org/repo/alphagov%2Fnotifications-functional-tests/requests
+    ;;
+  *)
+    echo "Not triggering dependent build for $TRAVIS_BRANCH"
+    ;;
+esac
