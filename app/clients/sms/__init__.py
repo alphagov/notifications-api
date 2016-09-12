e38fe14886aec6980839a03a0386a4bd98d547f3
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-09-12 16:24:06 +0100

    Updated SMS clients to throw ResponseException
    This will contain the response status code, text of the body plus any exception thrown
    
    - We also check the JSON of the response to ensure valid
    
    This should mean that all exceptions from the clients are caught and wrapped and can thus force retries on the tasks.

--- app/clients/sms/__init__.py
+++ app/clients/sms/__init__.py
@@ -8,6 +8,18 @@ class SmsClientException(ClientException):
     pass
 
 
+class SmsClientResponseException(ClientException):
+    '''
+    Base Exception for SmsClientsResponses
+    '''
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return "Message {}".format(self.message)
+
+
 class SmsClient(Client):
     '''
     Base Sms client for sending smss.
--- tests/app/public_contracts/test_POST_notification.py
+++ tests/app/public_contracts/test_POST_notification.py
@@ -5,7 +5,7 @@ from tests import create_authorization_header
 
 
 def test_post_sms_contract(client, mocker, sample_template):
-    mocker.patch('app.celery.tasks.send_sms.apply_async')
+    mocker.patch('app.celery.tasks.send_sms_to_provider.apply_async')
     mocker.patch('app.encryption.encrypt', return_value="something_encrypted")
 
     data = {
@@ -20,12 +20,11 @@ def test_post_sms_contract(client, mocker, sample_template):
         data=json.dumps(data),
         headers=[('Content-Type', 'application/json'), auth_header]
     )
-
     validate(response.get_data(as_text=True), 'POST_notification_return_sms.json')
 
 
 def test_post_email_contract(client, mocker, sample_email_template):
-    mocker.patch('app.celery.tasks.send_email.apply_async')
+    mocker.patch('app.celery.tasks.send_email_to_provider.apply_async')
     mocker.patch('app.encryption.encrypt', return_value="something_encrypted")
 
     data = {
