34bd17ce66a2d5d0ba43373001c95acf40504056
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-09-13 14:40:04 +0100

    Record time in seconds

--- app/clients/statsd/statsd_client.py
+++ app/clients/statsd/statsd_client.py
@@ -27,5 +27,5 @@ class StatsdClient(StatsClient):
 
     def timing_with_dates(self, stat, start, end, rate=1):
         if self.active:
-            delta = (start - end).total_seconds() * 1000
+            delta = (start - end).total_seconds()
             super(StatsClient, self).timing(stat, delta, rate)
