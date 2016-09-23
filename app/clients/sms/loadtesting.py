124487f204c5dedea471bd2c45ad8b929ff7fae0
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-09-23 10:31:18 +0100

    Fix from number on Load testing client

--- app/clients/sms/loadtesting.py
+++ app/clients/sms/loadtesting.py
@@ -18,7 +18,7 @@ class LoadtestingClient(FiretextClient):
         super(FiretextClient, self).__init__(*args, **kwargs)
         self.current_app = current_app
         self.api_key = config.config.get('LOADTESTING_API_KEY')
-        self.from_number = config.config.get('LOADTESTING_NUMBER')
+        self.from_number = config.config.get('FROM_NUMBER')
         self.name = 'loadtesting'
         self.url = "https://www.firetext.co.uk/api/sendsms/json"
         self.statsd_client = statsd_client
