c1fa5e156a87276ca6aacb0341928bf2f95364bb
Paul Craig <paul_craig_16@hotmail.com>
2016-11-21 15:16:21 +0000

    Append "Z" to DATETIME_FORMAT
    We're formally using the ISO 8601 UTC datetime format, and so the
    correct way to output the data is by appending the timezone.
    ("Z" in the case of UTC*).
    
    Unfortunately, Python's `datetime` formatting will just ignore the
    timezone part of the string on output, which means we just have to
    append the string "Z" to the end of all datetime strings we output.
    
    Should be fine, as we will only ever output UTC timestamps anyway.
    
    * https://en.wikipedia.org/wiki/ISO_8601#UTC

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ from app.clients.statsd.statsd_client import StatsdClient
 from app.encryption import Encryption
 
 
-DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 DATE_FORMAT = "%Y-%m-%d"
 
 db = SQLAlchemy()
