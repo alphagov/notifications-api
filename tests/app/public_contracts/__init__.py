7ae427c4ef84089551928b9bba809a467d729f49
Paul Craig <paul_craig_16@hotmail.com>
2016-11-22 11:17:28 +0000

    Use jsonschema validation + remove 'v2' schema
    Emualated the validation methods that exist [in the python-client](https://github.com/alphagov/notifications-python-client/blob/620e5f7014e4a431a7192cd617315e56b26c912f/integration_test/__init__.py).
    
    The `validate_v0` function loads json schemas from a local
    `/schemas` directory, whereas the new `validate` function (which
    we're going to use for our v2 API calls) uses the common
    `get_notification_response` python schema defined in
    "app/v2/notifications/notification_schemas.py".
    
    Removed the new `v2` schema from the last commit as it's no longer
    being used.
    
    Also, refactored common code in the GET and POST contract files
    so that making requests and converting responses to JSON are
    pulled out into common functions.

--- tests/app/public_contracts/__init__.py
+++ tests/app/public_contracts/__init__.py
@@ -2,15 +2,25 @@ import os
 
 from flask import json
 import jsonschema
+from jsonschema import Draft4Validator
 
 
-def validate(json_string, schema_filename):
-    schema_dir = os.path.join(os.path.dirname(__file__), 'schemas')
+def return_json_from_response(response):
+    return json.loads(response.get_data(as_text=True))
+
+
+def validate_v0(json_to_validate, schema_filename):
+    schema_dir = os.path.join(os.path.dirname(__file__), 'schemas/v0')
     resolver = jsonschema.RefResolver('file://' + schema_dir + '/', None)
     with open(os.path.join(schema_dir, schema_filename)) as schema:
         jsonschema.validate(
-            json.loads(json_string),
+            json_to_validate,
             json.load(schema),
             format_checker=jsonschema.FormatChecker(),
             resolver=resolver
         )
+
+
+def validate(json_to_validate, schema):
+    validator = Draft4Validator(schema)
+    validator.validate(json_to_validate, schema)
--- tests/app/public_contracts/test_GET_notification.py
+++ tests/app/public_contracts/test_GET_notification.py
@@ -1,10 +1,11 @@
-from . import validate
+from . import return_json_from_response, validate_v0, validate
 from app.models import ApiKey, KEY_TYPE_NORMAL
 from app.dao.api_key_dao import save_model_api_key
+from app.v2.notifications.notification_schemas import get_notification_response
 from tests import create_authorization_header
 
 
-def _get(client, notification, url):
+def _get_notification(client, notification, url):
     save_model_api_key(ApiKey(
         service=notification.service,
         name='api_key',
@@ -16,30 +17,42 @@ def _get(client, notification, url):
 
 
 def test_get_v2_notification(client, sample_notification):
-    response = _get(client, sample_notification, '/v2/notifications/{}'.format(sample_notification.id))
-    validate(response.get_data(as_text=True), 'GET_notification_return_email_v2.json')
+    response_json = return_json_from_response(_get_notification(
+        client, sample_notification, '/v2/notifications/{}'.format(sample_notification.id)
+    ))
+    validate(response_json, get_notification_response)
 
 
 def test_get_api_sms_contract(client, sample_notification):
-    response = _get(client, sample_notification, '/notifications/{}'.format(sample_notification.id))
-    validate(response.get_data(as_text=True), 'GET_notification_return_sms.json')
+    response_json = return_json_from_response(_get_notification(
+        client, sample_notification, '/notifications/{}'.format(sample_notification.id)
+    ))
+    validate_v0(response_json, 'GET_notification_return_sms.json')
 
 
 def test_get_api_email_contract(client, sample_email_notification):
-    response = _get(client, sample_email_notification, '/notifications/{}'.format(sample_email_notification.id))
-    validate(response.get_data(as_text=True), 'GET_notification_return_email.json')
+    response_json = return_json_from_response(_get_notification(
+        client, sample_email_notification, '/notifications/{}'.format(sample_email_notification.id)
+    ))
+    validate_v0(response_json, 'GET_notification_return_email.json')
 
 
 def test_get_job_sms_contract(client, sample_notification):
-    response = _get(client, sample_notification, '/notifications/{}'.format(sample_notification.id))
-    validate(response.get_data(as_text=True), 'GET_notification_return_sms.json')
+    response_json = return_json_from_response(_get_notification(
+        client, sample_notification, '/notifications/{}'.format(sample_notification.id)
+    ))
+    validate_v0(response_json, 'GET_notification_return_sms.json')
 
 
 def test_get_job_email_contract(client, sample_email_notification):
-    response = _get(client, sample_email_notification, '/notifications/{}'.format(sample_email_notification.id))
-    validate(response.get_data(as_text=True), 'GET_notification_return_email.json')
+    response_json = return_json_from_response(_get_notification(
+        client, sample_email_notification, '/notifications/{}'.format(sample_email_notification.id)
+    ))
+    validate_v0(response_json, 'GET_notification_return_email.json')
 
 
 def test_get_notifications_contract(client, sample_notification, sample_email_notification):
-    response = _get(client, sample_notification, '/notifications')
-    validate(response.get_data(as_text=True), 'GET_notifications_return.json')
+    response_json = return_json_from_response(_get_notification(
+        client, sample_notification, '/notifications'
+    ))
+    validate_v0(response_json, 'GET_notifications_return.json')
