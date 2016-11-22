e7a01e979b3afab5d6c2c9cdb186979a1a169e77
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-11-22 13:10:12 +0000

    Fixed merge conflicts

--- tests/app/notifications/test_process_notification.py
+++ tests/app/notifications/test_process_notification.py
@@ -154,7 +154,7 @@ def test_send_notification_to_queue(notify_db, notify_db_session,
 
 def test_send_notification_to_queue_throws_exception_deletes_notification(sample_notification, mocker):
     mocked = mocker.patch('app.celery.provider_tasks.deliver_sms.apply_async', side_effect=Boto3Error("EXPECTED"))
-    with pytest.raises(Boto3Error):
+    with pytest.raises(SendNotificationToQueueError):
         send_notification_to_queue(sample_notification, False)
         mocked.assert_called_once_with([(str(sample_notification.id))], queue='send-sms')
 
