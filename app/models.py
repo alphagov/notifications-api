2924f01f959f9f6c2ea79bafd73423a8360e68e4
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2016-12-15 16:59:03 +0000

    Fix for missing template IDs.
    - Problem was that on notification creation we pass the template ID not the template onto the new notification object
    - We then set the history object from this notification object by copying all the fields. This is OK at this point.
    - We then set the relationship on the history object based on the template, which we haven't passed in. We only passed the ID. This means that SQLAlchemy nulls the relationship, removing the template_id.
    - Later we update the history row when we send the message, this fixes the data. BUT if we ever have a send error, then this never happens and the template is never set on the history table.
    
    Fix:
    Set only the template ID when creating the history object.

--- app/models.py
+++ app/models.py
@@ -667,7 +667,6 @@ class NotificationHistory(db.Model):
     @classmethod
     def from_notification(cls, notification):
         history = cls(**{c.name: getattr(notification, c.name) for c in cls.__table__.columns})
-        history.template = notification.template
         return history
 
     def update_from_notification(self, notification):
