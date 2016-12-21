0e6d5ed5ebe7460aafa365f6b595124589436b80
Imdad Ahad <imdad.ahad@digital.cabinet-office.gov.uk>
2016-12-21 14:10:43 +0000

    Add dao method to switch providers with tests

--- app/dao/provider_details_dao.py
+++ app/dao/provider_details_dao.py
@@ -4,6 +4,7 @@ from sqlalchemy import asc
 from app.dao.dao_utils import transactional
 from app.models import ProviderDetails, ProviderDetailsHistory
 from app import db
+from flask import current_app
 
 
 def get_provider_details():
@@ -14,10 +15,38 @@ def get_provider_details_by_id(provider_details_id):
     return ProviderDetails.query.get(provider_details_id)
 
 
+def get_provider_details_by_identifier(identifier):
+    return ProviderDetails.query.filter_by(identifier=identifier).one()
+
+
+def get_alternative_sms_provider(identifier):
+    sms_providers = set(['firetext', 'mmg'])
+    selected_provider = None
+    try:
+        selected_provider = sms_providers.difference([identifier]).pop()
+    except KeyError:
+        current_app.logger.error('Could not get an alternative sms provider from list {} given {}').format(
+            sms_providers,
+            identifier
+        )
+    else:
+        return ProviderDetails.query.filter_by(
+            identifier=selected_provider
+        ).one()
+
+
 def get_provider_details_by_notification_type(notification_type):
     return ProviderDetails.query.filter_by(
         notification_type=notification_type
     ).order_by(asc(ProviderDetails.priority)).all()
+
+
+def get_current_provider(notification_type):
+    return ProviderDetails.query.filter_by(
+        notification_type=notification_type
+    ).order_by(
+        asc(ProviderDetails.priority)
+    ).first()
 
 
 @transactional
@@ -27,3 +56,36 @@ def dao_update_provider_details(provider_details):
     history = ProviderDetailsHistory.from_original(provider_details)
     db.session.add(provider_details)
     db.session.add(history)
+
+
+@transactional
+def dao_switch_sms_provider(identifier):
+    current_provider = get_current_provider('sms')
+    new_provider = get_alternative_sms_provider(identifier)
+
+    if current_provider.identifier != new_provider.identifier:
+        # Swap priority to change primary provider
+        if new_provider.priority > current_provider.priority:
+            new_provider.priority, current_provider.priority = current_provider.priority, new_provider.priority
+
+            current_app.logger.info('Switching provider from {} to {}'.format(
+                current_provider.identifier,
+                new_provider.identifier
+            ))
+
+            current_app.logger.info('Provider {} now updated with priority of {}'.format(
+                current_provider.identifier,
+                current_provider.priority
+            ))
+            current_app.logger.info('Provider {} now updated with priority of {}'.format(
+                new_provider.identifier,
+                new_provider.priority
+            ))
+
+        elif new_provider.priority == current_provider.priority:
+            current_provider.priority += 10
+
+        db.session.add_all([current_provider, new_provider])
+
+    else:
+        current_app.logger.info('Alternative provider {} is already activated'.format(new_provider.identifier))
