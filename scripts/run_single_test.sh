4e4afc5d0b5ca9a9c235b166ceacf82e774b5e8c
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2017-01-27 15:21:59 +0000

    Rename test with duplicate name

--- scripts/run_single_test.sh
+++ scripts/run_single_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # run a single unit test, pass in the unit test name for example: tests/app/service/test_rest.py::test_get_template_list
 source environment_test.sh
-py.test -s $@
+py.test $@
