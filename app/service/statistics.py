cffd0c2c5444413df33e74cad0083e6a1eb3cf4b
Chris Hill-Scott <me@quis.cc>
2017-02-08 13:16:41 +0000

    Remove weekly stats endpoints
    The weekly stats endpoint wont be used once
    https://github.com/alphagov/notifications-admin/pull/1109 has been
    merged.
    
    It has been replaced with a new monthly endpoint in
    https://github.com/alphagov/notifications-api/pull/807

--- app/service/statistics.py
+++ app/service/statistics.py
@@ -15,20 +15,6 @@ def format_statistics(statistics):
     return counts
 
 
-def format_weekly_notification_stats(statistics, service_created_at):
-    preceeding_monday = (service_created_at - timedelta(days=service_created_at.weekday()))
-    # turn a datetime into midnight that day http://stackoverflow.com/a/1937636
-    preceeding_monday_midnight = datetime.combine(preceeding_monday.date(), datetime.min.time())
-    week_dict = {
-        week: create_zeroed_stats_dicts()
-        for week in _weeks_for_range(preceeding_monday_midnight, datetime.utcnow())
-    }
-    for row in statistics:
-        _update_statuses_from_row(week_dict[row.week_start][row.notification_type], row)
-
-    return week_dict
-
-
 def create_zeroed_stats_dicts():
     return {
         template_type: {
@@ -43,11 +29,3 @@ def _update_statuses_from_row(update_dict, row):
         update_dict['delivered'] += row.count
     elif row.status in ('failed', 'technical-failure', 'temporary-failure', 'permanent-failure'):
         update_dict['failed'] += row.count
-
-
-def _weeks_for_range(start, end):
-    """
-    Generator that yields dates from `start` to `end`, in 7 day intervals. End is inclusive.
-    """
-    infinite_date_generator = (start + timedelta(days=i) for i in itertools.count(step=7))
-    return itertools.takewhile(lambda x: x <= end, infinite_date_generator)
