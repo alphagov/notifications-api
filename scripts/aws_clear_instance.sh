2e4b09154ed599c90774391398aa0176c96f33d1
Imdad Ahad <imdad.ahad@digital.cabinet-office.gov.uk>
2017-02-14 11:37:47 +0000

    Remove verbose logging of directory creation

--- scripts/aws_clear_instance.sh
+++ scripts/aws_clear_instance.sh
@@ -5,6 +5,6 @@ echo "Removing application and dependencies"
 if [ -d "/home/notify-app/notifications-api" ]; then
     # Remove and re-create the directory
     rm -rf /home/notify-app/notifications-api
-    mkdir -vp /home/notify-app/notifications-api
+    mkdir -p /home/notify-app/notifications-api
 fi
 
