c633e193b0602c8c98c465dd0e3484b4fcc4abcb
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-03-31 15:54:29 +0100

    Update config to enable performance platform

--- app/config.py
+++ app/config.py
@@ -52,9 +52,7 @@ class Config(object):
     EXPIRE_CACHE_IN_SECONDS = 600
 
     # Performance platform
-    PERFORMANCE_PLATFORM_ENABLED = os.getenv('PERFORMANCE_PLATFORM_ENABLED') == '1'
-    PERFORMANCE_PLATFORM_URL = 'https://www.performance.service.gov.uk/data/govuk-notify/notifications'
-    PERFORMANCE_PLATFORM_TOKEN = os.getenv('PERFORMANCE_PLATFORM_TOKEN')
+    PERFORMANCE_PLATFORM_ENABLED = False
 
     # Logging
     DEBUG = False
@@ -244,6 +242,9 @@ class Live(Config):
     FROM_NUMBER = '40604'
     FUNCTIONAL_TEST_PROVIDER_SERVICE_ID = '6c1d81bb-dae2-4ee9-80b0-89a4aae9f649'
     FUNCTIONAL_TEST_PROVIDER_SMS_TEMPLATE_ID = 'ba9e1789-a804-40b8-871f-cc60d4c1286f'
+    PERFORMANCE_PLATFORM_ENABLED = True
+    PERFORMANCE_PLATFORM_URL = 'https://www.performance.service.gov.uk/data/govuk-notify/notifications'
+    PERFORMANCE_PLATFORM_TOKEN = os.getenv('PERFORMANCE_PLATFORM_TOKEN')
 
 
 class CloudFoundryConfig(Config):
