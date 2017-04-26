4c37c8bdbb2ae74f0ae9b5b44d5b39930e785dd9
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2017-04-26 15:31:25 +0100

    New query to get billing data per month.

--- app/dao/notification_usage_dao.py
+++ app/dao/notification_usage_dao.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from decimal import Decimal
 
 from sqlalchemy import func
@@ -11,6 +12,7 @@ from app.models import (NotificationHistory,
                         SMS_TYPE,
                         EMAIL_TYPE)
 from app.statsd_decorators import statsd
+from app.utils import get_london_month_from_utc_column
 
 
 @statsd(namespace="dao")
@@ -31,21 +33,26 @@ def get_yearly_billing_data(service_id, year):
 
 
 def billing_data_filter(notification_type, start_date, end_date, service_id):
-    return [NotificationHistory.notification_type == notification_type,
-            NotificationHistory.created_at >= start_date,
-            NotificationHistory.created_at < end_date,
-            NotificationHistory.service_id == service_id,
-            NotificationHistory.status.in_(NOTIFICATION_STATUS_TYPES_BILLABLE),
-            NotificationHistory.key_type != KEY_TYPE_TEST
-            ]
+    return [
+        NotificationHistory.notification_type == notification_type,
+        NotificationHistory.created_at >= start_date,
+        NotificationHistory.created_at < end_date,
+        NotificationHistory.service_id == service_id,
+        NotificationHistory.status.in_(NOTIFICATION_STATUS_TYPES_BILLABLE),
+        NotificationHistory.key_type != KEY_TYPE_TEST
+    ]
 
 
 def email_billing_data_query(service_id, start_date, end_date):
-    result = db.session.query(func.count(NotificationHistory.id),
-                              NotificationHistory.notification_type,
-                              "0"
-                              ).filter(*billing_data_filter(EMAIL_TYPE, start_date, end_date, service_id)
-                                       ).group_by(NotificationHistory.notification_type).first()
+    result = db.session.query(
+        func.count(NotificationHistory.id),
+        NotificationHistory.notification_type,
+        "0"
+    ).filter(
+        *billing_data_filter(EMAIL_TYPE, start_date, end_date, service_id)
+    ).group_by(
+        NotificationHistory.notification_type
+    ).first()
     if not result:
         return 0, EMAIL_TYPE, Decimal("0")
     else:
@@ -53,11 +60,15 @@ def email_billing_data_query(service_id, start_date, end_date):
 
 
 def sms_billing_data_query(rate, service_id, start_date, end_date):
-    result = db.session.query(func.sum(NotificationHistory.billable_units),
-                              NotificationHistory.notification_type,
-                              rate
-                              ).filter(*billing_data_filter(SMS_TYPE, start_date, end_date, service_id)
-                                       ).group_by(NotificationHistory.notification_type).first()
+    result = db.session.query(
+        func.sum(NotificationHistory.billable_units),
+        NotificationHistory.notification_type,
+        rate
+    ).filter(
+        *billing_data_filter(SMS_TYPE, start_date, end_date, service_id)
+    ).group_by(
+        NotificationHistory.notification_type
+    ).first()
     if not result:
         return 0, SMS_TYPE, Decimal("0")
     else:
@@ -67,3 +78,50 @@ def sms_billing_data_query(rate, service_id, start_date, end_date):
 def get_rates_for_year(start_date, end_date, notification_type):
     return Rate.query.filter(Rate.valid_from >= start_date, Rate.valid_from < end_date,
                              Rate.notification_type == notification_type).order_by(Rate.valid_from).all()
+
+
+def sms_billing_data_per_month_query(rate, service_id, start_date, end_date):
+    month = get_london_month_from_utc_column(NotificationHistory.created_at)
+    return db.session.query(
+        month,
+        func.sum(NotificationHistory.billable_units),
+        NotificationHistory.notification_type,
+        rate
+    ).filter(
+        *billing_data_filter(SMS_TYPE, start_date, end_date, service_id)
+    ).group_by(
+        NotificationHistory.notification_type, month
+    ).order_by(
+        month
+    ).all()
+
+
+def email_billing_data_per_month_query(rate, service_id, start_date, end_date):
+    month = get_london_month_from_utc_column(NotificationHistory.created_at)
+    return db.session.query(
+        month,
+        func.count(NotificationHistory.id),
+        NotificationHistory.notification_type,
+        rate
+    ).filter(
+        *billing_data_filter(EMAIL_TYPE, start_date, end_date, service_id)
+    ).group_by(
+        NotificationHistory.notification_type, month
+    ).order_by(
+        month
+    ).all()
+
+
+@statsd(namespace="dao")
+def get_notification_billing_data_per_month(service_id, year):
+    start_date, end_date = get_financial_year(year)
+    rates = get_rates_for_year(start_date, end_date, SMS_TYPE)
+
+    result = []
+    for r, n in zip(rates, rates[1:]):
+        result.extend(sms_billing_data_per_month_query(str(r.rate), service_id, r.valid_from, n.valid_from))
+    result.extend(sms_billing_data_per_month_query(str(rates[-1].rate), service_id, rates[-1].valid_from, end_date))
+
+    result.extend(email_billing_data_per_month_query("0", service_id, start_date, end_date))
+
+    return [(datetime.strftime(x[0], "%B"), x[1:]) for x in result]
