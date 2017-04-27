bedbd8e21fff019dec3f72c9dc4cdd43132faeab
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2017-04-27 12:15:47 +0100

    Removed unneeded test case

--- tests/app/dao/test_provider_details_dao.py
+++ tests/app/dao/test_provider_details_dao.py
@@ -39,7 +39,6 @@ def test_can_get_sms_non_international_providers(restore_provider_details):
     sms_providers = get_provider_details_by_notification_type('sms')
     assert len(sms_providers) == 3
     assert all('sms' == prov.notification_type for prov in sms_providers)
-    assert all(not prov.supports_international for prov in sms_providers)
 
 
 def test_can_get_sms_international_providers(restore_provider_details):
