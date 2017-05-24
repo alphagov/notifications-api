640ab665d04355d28e7c4c1a87c5d48ea5e364e6
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-05-24 13:22:36 +0100

    Update migration script for service permissions

--- migrations/versions/0086_migrate_existing_svc_perms.py
+++ migrations/versions/0086_migrate_existing_svc_perms.py
@@ -1,13 +1,13 @@
 """empty message
 
-Revision ID: 0086_migrate_default_svc_perms
+Revision ID: 0086_migrate_existing_svc_perms
 Revises: 0085_update_incoming_to_inbound
 Create Date: 2017-05-23 18:13:03.532095
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '0086_migrate_default_svc_perms'
+revision = '0086_migrate_existing_svc_perms'
 down_revision = '0085_update_incoming_to_inbound'
 
 from alembic import op
@@ -20,8 +20,17 @@ def upgrade():
             "id NOT IN (SELECT service_id FROM service_permissions "\
             "WHERE service_id=id AND permission='{0}')".format(permission)
 
+    def get_values_from_flag(permission, flag):
+        return "SELECT id, '{0}' FROM services WHERE "\
+            "{1} AND id NOT IN (SELECT service_id FROM service_permissions "\
+            "WHERE service_id=id AND permission='{0}')".format(permission, flag)
+
     op.execute("INSERT INTO service_permissions (service_id, permission) {}".format(get_values('sms')))
     op.execute("INSERT INTO service_permissions (service_id, permission) {}".format(get_values('email')))
+    op.execute("INSERT INTO service_permissions (service_id, permission) {}".format(
+        get_values_from_flag('letter', 'can_send_letters')))
+    op.execute("INSERT INTO service_permissions (service_id, permission) {}".format(
+        get_values_from_flag('international_sms', 'can_send_international_sms')))
 
 
 def downgrade():
