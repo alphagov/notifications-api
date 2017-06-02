400096520d582a51855939c48da357152e2e1bec
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2017-06-02 16:51:27 +0100

    Not null the provider column on the inbound SMS table.

---
+++ migrations/versions/0093_notnull_inbound_provider.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 0093_notnull_inbound_provider
+Revises: 0092_populate_inbound_provider
+Create Date: 2017-06-02 16:50:11.698423
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '0093_notnull_inbound_provider'
+down_revision = '0092_populate_inbound_provider'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column('inbound_sms', 'provider',
+               existing_type=sa.VARCHAR(),
+               nullable=False)
+
+
+def downgrade():
+    op.alter_column('inbound_sms', 'provider',
+               existing_type=sa.VARCHAR(),
+               nullable=True)
