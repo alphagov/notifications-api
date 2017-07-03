4d41da22a45695b6d6c37b49dfe73404eaef90f9
Imdad Ahad <imdad.ahad@digital.cabinet-office.gov.uk>
2017-07-03 12:12:38 +0100

    Use correct schema for dumping a notification with personalisation

--- app/service/rest.py
+++ app/service/rest.py
@@ -326,7 +326,7 @@ def get_notification_for_service(service_id, notification_id):
 def search_for_notification_by_to_field(service_id, search_term, statuses):
     results = notifications_dao.dao_get_notifications_by_to_field(service_id, search_term, statuses)
     return jsonify(
-        notifications=notification_with_personalisation_schema.dump(results, many=True).data
+        notifications=notification_with_template_schema.dump(results, many=True).data
     ), 200
 
 
