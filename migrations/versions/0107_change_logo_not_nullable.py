1ca59c4b445c89969eb375d45d6b147952f52ee3
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-07-11 12:38:30 +0100

    Renamed migration script

--- migrations/versions/0107_change_logo_not_nullable.py
+++ migrations/versions/0107_change_logo_not_nullable.py
@@ -1,14 +1,14 @@
 """empty message
 
-Revision ID: 0106_change_logo_not_nullable
-Revises: 0105_opg_letter_org
+Revision ID: 0107_change_logo_not_nullable
+Revises: 0106_null_noti_status
 Create Date: 2017-07-06 10:14:35.188404
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '0106_change_logo_not_nullable'
-down_revision = '0105_opg_letter_org'
+revision = '0107_change_logo_not_nullable'
+down_revision = '0106_null_noti_status'
 
 from alembic import op
 import sqlalchemy as sa
