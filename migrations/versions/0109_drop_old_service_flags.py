beca03a39c647cbbe0ae7c388614fe655384f774
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-07-12 13:10:44 +0100

    Add migration script to drop service flags

---
+++ migrations/versions/0109_drop_old_service_flags.py
@@ -0,0 +1,28 @@
+"""empty message
+
+Revision ID: 0109_drop_old_service_flags
+Revises: 0108_change_logo_not_nullable
+Create Date: 2017-07-12 13:35:45.636618
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '0109_drop_old_service_flags'
+down_revision = '0108_change_logo_not_nullable'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+def upgrade():
+    op.drop_column('services', 'can_send_letters')
+    op.drop_column('services', 'can_send_international_sms')
+    op.drop_column('services_history', 'can_send_letters')
+    op.drop_column('services_history', 'can_send_international_sms')
+
+
+def downgrade():
+    op.add_column('services_history', sa.Column('can_send_international_sms', sa.BOOLEAN(), server_default=sa.text('false'), autoincrement=False, nullable=False))
+    op.add_column('services_history', sa.Column('can_send_letters', sa.BOOLEAN(), server_default=sa.text('false'), autoincrement=False, nullable=False))
+    op.add_column('services', sa.Column('can_send_international_sms', sa.BOOLEAN(), server_default=sa.text('false'), autoincrement=False, nullable=False))
+    op.add_column('services', sa.Column('can_send_letters', sa.BOOLEAN(), server_default=sa.text('false'), autoincrement=False, nullable=False))
