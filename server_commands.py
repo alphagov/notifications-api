7db1bfbb774ca222855cbaf47366477d7134a675
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2017-07-25 14:45:59 +0100

    remove credstash

--- server_commands.py
+++ server_commands.py
@@ -1,7 +1,6 @@
 from flask.ext.script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app import (create_app, db, commands)
-from credstash import getAllSecrets
 import os
 
 default_env_file = '/home/ubuntu/environment'
@@ -10,10 +9,6 @@ environment = 'live'
 if os.path.isfile(default_env_file):
     with open(default_env_file, 'r') as environment_file:
         environment = environment_file.readline().strip()
-
-# On AWS get secrets and export to env, skip this on Cloud Foundry
-if os.getenv('VCAP_SERVICES') is None:
-    os.environ.update(getAllSecrets(region="eu-west-1"))
 
 from app.config import configs
 
