de41fde0d37e3f52b316bcda74445c07d04eb28b
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2017-07-28 16:09:49 +0100

    Revert "remove credstash"
    This reverts commit 7db1bfbb774ca222855cbaf47366477d7134a675.

--- db.py
+++ db.py
@@ -1,8 +1,12 @@
 from flask.ext.script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-
 from app import create_app, db
+from credstash import getAllSecrets
+import os
 
+# On AWS get secrets and export to env, skip this on Cloud Foundry
+if os.getenv('VCAP_SERVICES') is None:
+    os.environ.update(getAllSecrets(region="eu-west-1"))
 
 application = create_app()
 
