9c212e78af2b0e2c17b4375581226660b93f9837
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2017-07-31 11:11:00 +0100

    Revert "Revert "remove credstash""
    This reverts commit de41fde0d37e3f52b316bcda74445c07d04eb28b.

--- aws_run_celery.py
+++ aws_run_celery.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
-from app import notify_celery, create_app
-from credstash import getAllSecrets
-import os
-
-# On AWS get secrets and export to env, skip this on Cloud Foundry
-if os.getenv('VCAP_SERVICES') is None:
-    os.environ.update(getAllSecrets(region="eu-west-1"))
+from app import create_app
 
 application = create_app("delivery")
 application.app_context().push()
