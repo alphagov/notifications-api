6f1841599757612b05bbc03c476a2799a7002e3c
Chris Hill-Scott <me@quis.cc>
2017-08-14 13:54:18 +0100

    Fix order of DB migrations
    More migrations got merged in between me making 0114 and merging the PR
    that contained 0114.
    
    If we named the files as just 0114.py then this would get flagged as a
    merge conflict…

--- migrations/versions/0116_another_letter_org.py
+++ migrations/versions/0116_another_letter_org.py
@@ -1,14 +1,14 @@
 """empty message
 
-Revision ID: 0114_another_letter_org
-Revises: 0113_job_created_by_nullable
+Revision ID: 0116_another_letter_org
+Revises: 0115_add_inbound_numbers
 Create Date: 2017-06-29 12:44:16.815039
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '0114_another_letter_org'
-down_revision = '0113_job_created_by_nullable'
+revision = '0116_another_letter_org'
+down_revision = '0115_add_inbound_numbers'
 
 from alembic import op
 
