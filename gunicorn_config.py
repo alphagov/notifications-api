291071273e892712a565bfa48f721904094c3c39
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2017-09-22 15:34:43 +0100

    Fix PEP8

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,8 @@
+import sys
+import traceback
+
+
 def worker_abort(worker):
     worker.log.info("worker received ABORT")
-    import sys, traceback
     for threadId, stack in sys._current_frames().items():
         worker.log.info(''.join(traceback.format_stack(stack)))
