02ee072251a3b1ac24d468c84cb730655e4f77b5
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-11-06 18:19:02 +0000

    Refactor inbound sms dao
    Added function for paginated results

--- app/dao/inbound_sms_dao.py
+++ app/dao/inbound_sms_dao.py
@@ -2,7 +2,8 @@ from datetime import (
     timedelta,
     datetime
 )
-
+from flask import current_app
+from sqlalchemy import desc
 
 from app import db
 from app.dao.dao_utils import transactional
@@ -29,6 +30,34 @@ def dao_get_inbound_sms_for_service(service_id, limit=None, user_number=None):
         q = q.limit(limit)
 
     return q.all()
+
+
+def dao_get_paginated_inbound_sms_for_service(
+    service_id,
+    user_number=None,
+    older_than=None,
+    page=1,
+    page_size=None
+):
+    if page_size is None:
+        page_size = current_app.config['PAGE_SIZE']
+
+    filters = [InboundSms.service_id == service_id]
+
+    if older_than is not None:
+        older_than_created_at = db.session.query(
+            InboundSms.created_at).filter(InboundSms.id == older_than).as_scalar()
+        filters.append(InboundSms.created_at < older_than_created_at)
+
+    if user_number:
+        filters.append(InboundSms.user_number == user_number)
+
+    query = InboundSms.query.filter(*filters)
+
+    return query.order_by(desc(InboundSms.created_at)).paginate(
+        page=page,
+        per_page=page_size
+    ).items
 
 
 def dao_count_inbound_sms_for_service(service_id):
