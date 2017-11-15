17d1cfe7588c2cf5b04c048851e3c96ad6bb2b22
Richard Chapman <richard.chapman@digital.cabinet-office.gov.uk>
2017-11-15 16:00:13 +0000

    Reverted Config on SQLALCHEMY_ECHO
    Used for debugging not needed for production so reverted.

--- app/config.py
+++ app/config.py
@@ -307,7 +307,7 @@ class Config(object):
 
 class Development(Config):
     NOTIFY_LOG_PATH = 'application.log'
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = False
     NOTIFY_EMAIL_DOMAIN = 'notify.tools'
     CSV_UPLOAD_BUCKET_NAME = 'development-notifications-csv-upload'
     DVLA_RESPONSE_BUCKET_NAME = 'notify.tools-ftp'
