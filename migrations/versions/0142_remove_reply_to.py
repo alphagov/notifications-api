c2648d5c9e71a99506be07ca58330a40d8c152b5
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2017-11-20 14:40:24 +0000

    Remove Services.reply_to_email_address and Services.letter_contact_block

---
+++ migrations/versions/0142_remove_reply_to.py
@@ -0,0 +1,27 @@
+"""
+
+Revision ID: 0142_remove_reply_to
+Revises: 0141_remove_unused
+Create Date: 2017-11-20 14:35:16.826398
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '0142_remove_reply_to'
+down_revision = '0141_remove_unused'
+
+
+def upgrade():
+    op.drop_column('services', 'letter_contact_block')
+    op.drop_column('services', 'reply_to_email_address')
+    op.drop_column('services_history', 'letter_contact_block')
+    op.drop_column('services_history', 'reply_to_email_address')
+
+
+def downgrade():
+    op.add_column('services_history', sa.Column('reply_to_email_address', sa.TEXT(), autoincrement=False, nullable=True))
+    op.add_column('services_history', sa.Column('letter_contact_block', sa.TEXT(), autoincrement=False, nullable=True))
+    op.add_column('services', sa.Column('reply_to_email_address', sa.TEXT(), autoincrement=False, nullable=True))
+    op.add_column('services', sa.Column('letter_contact_block', sa.TEXT(), autoincrement=False, nullable=True))
