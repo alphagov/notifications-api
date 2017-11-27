e1175542b70111084c199c590efd9abe74592460
venusbb <venus.bailey@digital.cabinet-office.gov.uk>
2017-11-27 14:05:04 +0000

    add reply-to-text to send notification to service users

--- app/service/sender.py
+++ app/service/sender.py
@@ -23,7 +23,8 @@ def send_notification_to_service_users(service_id, template_id, personalisation=
             personalisation=personalisation,
             notification_type=template.template_type,
             api_key_id=None,
-            key_type=KEY_TYPE_NORMAL
+            key_type=KEY_TYPE_NORMAL,
+            reply_to_text=notify_service.get_default_reply_to_email_address()
         )
         send_notification_to_queue(notification, False, queue=QueueNames.NOTIFY)
 
