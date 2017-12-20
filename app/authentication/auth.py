e8967c5b056f3fd15a21f8cc394dc3c264fee90f
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2017-12-20 14:40:07 +0000

    tweak log output

--- app/authentication/auth.py
+++ app/authentication/auth.py
@@ -92,7 +92,7 @@ def requires_auth():
         g.service_id = api_key.service_id
         _request_ctx_stack.top.authenticated_service = service
         _request_ctx_stack.top.api_user = api_key
-        current_app.logger.info('Succesful login for service {} with api key {}, using client'.format(
+        current_app.logger.info('API authorised for service {} with api key {}, using client {}'.format(
             service.id,
             api_key.id,
             request.headers.get('User-Agent')
