2470faf04b051a50f5c4cff00ea0e61c16f8f321
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2018-01-02 14:25:40 +0000

    Remove unused scripts
    These were referenced by appspec and have not been used since we
    migrated to paas, so they can be removed.

--- scripts/aws_change_ownership.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-echo "Chown application to be owned by notify-app"
-
-cd /home/notify-app/;
-chown -R notify-app:govuk-notify-applications notifications-api
--- scripts/aws_install_dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-echo "Install dependencies"
-
-cd /home/notify-app/notifications-api;
-pip3 install -r /home/notify-app/notifications-api/requirements.txt
