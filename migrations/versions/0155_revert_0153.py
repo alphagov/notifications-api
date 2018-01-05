f34a380f7fbcde70b24106a0a2afbc0ad745c7ce
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2018-01-05 16:00:29 +0000

    Fix file name

--- migrations/versions/0155_revert_0153.py
+++ migrations/versions/0155_revert_0153.py
@@ -1,14 +1,14 @@
 """
 
-Revision ID: 0154_revert_0153
-Revises: 0153_add_is_letter_contact_blank
+Revision ID: 0155_revert_0153
+Revises: 0154_nullable_is_blank
 Create Date: 2018-01-05 14:09:21.200102
 
 """
 from alembic import op
 
-revision = '0154_revert_0153'
-down_revision = '0153_add_is_letter_contact_blank'
+revision = '0155_revert_0153'
+down_revision = '0154_nullable_is_blank'
 
 
 def upgrade():
