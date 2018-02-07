6a79eedbcec5bf9a68bc1954c59ee47dbd79bc83
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2018-02-07 14:43:09 +0000

    Add Organisation DAO

---
+++ app/dao/organisation_dao.py
@@ -0,0 +1,25 @@
+from app import db
+from app.dao.dao_utils import transactional
+from app.models import Organisation
+
+
+def dao_get_organisations():
+    return Organisation.query.order_by(
+        Organisation.active.desc(), Organisation.name.asc()
+    ).all()
+
+
+def dao_get_organisation_by_id(organisation_id):
+    return Organisation.query.filter_by(id=organisation_id).one()
+
+
+@transactional
+def dao_create_organisation(organisation):
+    db.session.add(organisation)
+
+
+@transactional
+def dao_update_organisation(organisation, **kwargs):
+    for key, value in kwargs.items():
+        setattr(organisation, key, value)
+    db.session.add(organisation)
