12046ee85ac16185f7b4b3e1f2ca8c9f130bc060
Rebecca Law <servingUpAces@gmail.com>
2018-02-27 13:46:23 +0000

    There endpoint to check the token of an invitation for services and organisations have been merged. This PR deletes the old endpoints.

--- app/__init__.py
+++ app/__init__.py
@@ -109,7 +109,6 @@ def register_blueprint(application):
     from app.billing.rest import billing_blueprint
     from app.organisation.rest import organisation_blueprint
     from app.organisation.invite_rest import organisation_invite_blueprint
-    from app.organisation.accept_organisation_invite import accept_organisation_invite_blueprint
 
     service_blueprint.before_request(requires_admin_auth)
     application.register_blueprint(service_blueprint, url_prefix='/service')
@@ -185,9 +184,6 @@ def register_blueprint(application):
 
     organisation_invite_blueprint.before_request(requires_admin_auth)
     application.register_blueprint(organisation_invite_blueprint)
-
-    accept_organisation_invite_blueprint.before_request(requires_admin_auth)
-    application.register_blueprint(accept_organisation_invite_blueprint)
 
 
 def register_v2_blueprints(application):
