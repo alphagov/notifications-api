cf2a506b3bb6c768bbebc9e61d7aa61123d1085d
Richard Chapman <richard.chapman@digital.cabinet-office.gov.uk>
2018-03-06 18:20:45 +0000

    Removed the import and directly used json.decoder.JSONDecodeError as it was failing on Jenkins.

--- tests/app/template/test_rest.py
+++ tests/app/template/test_rest.py
@@ -3,7 +3,6 @@ import json
 import random
 import string
 from datetime import datetime, timedelta
-from json.decoder import JSONDecodeError
 
 import botocore
 import pytest
@@ -996,7 +995,7 @@ def test_preview_letter_template_precompiled_png_file_type(
                 file_type='png'
             )
 
-            with pytest.raises(JSONDecodeError):
+            with pytest.raises(json.decoder.JSONDecodeError):
                 mock_post.last_request.json()
             assert mock_get_letter_pdf.called_once_with(notification)
             assert base64.b64decode(resp['content']) == png_content
@@ -1045,7 +1044,7 @@ def test_preview_letter_template_precompiled_png_template_preview_500_error(
 
             )
 
-            with pytest.raises(JSONDecodeError):
+            with pytest.raises(json.decoder.JSONDecodeError):
                 mock_post.last_request.json()
 
 
@@ -1091,5 +1090,5 @@ def test_preview_letter_template_precompiled_png_template_preview_400_error(
                 _expected_status=500
             )
 
-            with pytest.raises(JSONDecodeError):
+            with pytest.raises(json.decoder.JSONDecodeError):
                 mock_post.last_request.json()
