bfc4343b0e1310e3769168fddad33da2d6fd177e
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2018-10-04 15:52:51 +0100

    remove pip-accel and make sure commands work if you're in a venv
    remove pip-accel - it's not been updated in two years, and pins our
    version of pip to a version that is several breaking changes old.
    
    make sure commands work if you're already in a venv - mostly by
    checking for presence of $VIRTUAL_ENV, and ensuring we use the correct
    pip to install packages. Also clean up the commands a bit.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -20,17 +20,7 @@ function display_result {
   fi
 }
 
-if [ ! $VIRTUAL_ENV ]; then
-  virtualenv -p python3 ./venv
-  . ./venv/bin/activate
-fi
-
-# we need the version file to exist otherwise the app will blow up
-make generate-version-file
-
-make install-pycurl
-# Install Python development dependencies
-pip3 install -r requirements_for_test.txt
+make build
 
 # Create Postgres databases
 createdb notification_api
