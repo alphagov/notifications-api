821a7ccbac916d5d0c9201f223ee26058a346324
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2019-01-31 15:08:23 +0000

    Update requirements with flake8 and subdependencies and fix over-indents

--- tests/app/accept_invite/test_accept_invite_rest.py
+++ tests/app/accept_invite/test_accept_invite_rest.py
@@ -9,19 +9,19 @@ from tests import create_authorization_header
 
 @pytest.mark.parametrize('invitation_type', ['service', 'organisation'])
 def test_validate_invitation_token_for_expired_token_returns_400(client, invitation_type):
-        with freeze_time('2016-01-01T12:00:00'):
-            token = generate_token(str(uuid.uuid4()), current_app.config['SECRET_KEY'],
-                                   current_app.config['DANGEROUS_SALT'])
-        url = '/invite/{}/{}'.format(invitation_type, token)
-        auth_header = create_authorization_header()
-        response = client.get(url, headers=[('Content-Type', 'application/json'), auth_header])
+    with freeze_time('2016-01-01T12:00:00'):
+        token = generate_token(str(uuid.uuid4()), current_app.config['SECRET_KEY'],
+                               current_app.config['DANGEROUS_SALT'])
+    url = '/invite/{}/{}'.format(invitation_type, token)
+    auth_header = create_authorization_header()
+    response = client.get(url, headers=[('Content-Type', 'application/json'), auth_header])
 
-        assert response.status_code == 400
-        json_resp = json.loads(response.get_data(as_text=True))
-        assert json_resp['result'] == 'error'
-        assert json_resp['message'] == {'invitation': [
-            'Your invitation to GOV.UK Notify has expired. '
-            'Please ask the person that invited you to send you another one']}
+    assert response.status_code == 400
+    json_resp = json.loads(response.get_data(as_text=True))
+    assert json_resp['result'] == 'error'
+    assert json_resp['message'] == {'invitation': [
+        'Your invitation to GOV.UK Notify has expired. '
+        'Please ask the person that invited you to send you another one']}
 
 
 @pytest.mark.parametrize('invitation_type', ['service', 'organisation'])
