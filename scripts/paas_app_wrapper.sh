7d9cd58e89afaf1202cea7d5045f2e0339db1a97
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2019-04-10 15:19:46 +0100

    rename public-api to api
    we don't use the public-api bit anywhere - even cloudwatch overwrites
    based on CW_APP_NAME (which we can get rid of as this distinction is
    gone)

--- scripts/paas_app_wrapper.sh
+++ scripts/paas_app_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 case $NOTIFY_APP_NAME in
-  public-api)
+  api)
     unset GUNICORN_CMD_ARGS
     scripts/run_app_paas.sh gunicorn -c /home/vcap/app/gunicorn_config.py application
     ;;
