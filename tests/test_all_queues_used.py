36672e56fa7e6c0c69409a649e2ea0d274746efb
karlchillmaid <karl.chillmaid@digital.cabinet-office.gov.uk>
2019-09-19 15:43:39 +0100

    Replace isn't with is not

--- tests/test_all_queues_used.py
+++ tests/test_all_queues_used.py
@@ -13,6 +13,6 @@ def test_queue_names_set_in_paas_app_wrapper():
                 end_of_queue_names = line.find('2>') if '2>' in line else len(line)
                 watched_queues.update({q.strip() for q in line[start_of_queue_names:end_of_queue_names].split(',')})
 
-        # ses-callbacks isn't used in api (only used in SNS lambda)
+        # ses-callbacks is not used in api (only used in SNS lambda)
         ignored_queues = {'ses-callbacks'}
         assert watched_queues == set(QueueNames.all_queues()) | ignored_queues
