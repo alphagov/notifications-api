c9cfd60708865e0b634a38ae7d7f7bcff90518aa
karlchillmaid <karl.chillmaid@digital.cabinet-office.gov.uk>
2019-09-19 15:23:11 +0100

    Replace don't with do not

--- migrations/env.py
+++ migrations/env.py
@@ -29,8 +29,8 @@ def run_migrations_offline():
 
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
+    here as well. By skipping the Engine creation
+    we do not need a DBAPI to be available.
 
     Calls to context.execute() here emit the given string to the
     script output.
