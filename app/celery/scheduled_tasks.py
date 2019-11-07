5aaf5cd5888a9233eb3048535ce75b8662a2f3c9
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2019-11-07 15:01:23 +0000

    Add the missing format for the log message when a missing row is processed.

--- app/celery/scheduled_tasks.py
+++ app/celery/scheduled_tasks.py
@@ -245,5 +245,6 @@ def check_for_missing_rows_in_completed_jobs():
             recipient_csv, template = get_recipient_csv_and_template(job)
             for row in recipient_csv.get_rows():
                 if row.index == row_to_process.missing_row:
-                    current_app.logger.info("Processing missing row: {} for job: {}")
+                    current_app.logger.info(
+                        "Processing missing row: {} for job: {}".format(row_to_process.missing_row, job.id))
                     process_row(row, template, job, job.service)
