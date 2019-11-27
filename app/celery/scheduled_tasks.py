853df6fbfbe40a845cdb609247adf91c47986d5b
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2019-11-27 13:26:53 +0000

    Fix reference to old time frame for task.

--- app/celery/scheduled_tasks.py
+++ app/celery/scheduled_tasks.py
@@ -191,7 +191,7 @@ def replay_created_notifications():
         for n in notifications_to_resend:
             send_notification_to_queue(notification=n, research_mode=n.service.research_mode)
 
-    # if the letter has not be send after 4 hours + 15 minutes, then create a zendesk ticket
+    # if the letter has not be send after an hour, then create a zendesk ticket
     letters = letters_missing_from_sending_bucket(resend_created_notifications_older_than)
 
     if len(letters) > 0:
