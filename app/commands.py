d506451d4fb4db49c606a1e59a9cbc845f4aa74d
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2020-06-24 12:16:59 +0100

    Include template id, too

--- app/commands.py
+++ app/commands.py
@@ -790,7 +790,7 @@ def get_notification_and_service_ids_for_letters_that_failed_to_print(file_name)
 
 def get_letters_data_from_references(notification_references):
     sql = """
-        SELECT id, service_id, reference, job_id, created_at
+        SELECT id, service_id, template_id, reference, job_id, created_at
         FROM notifications
         WHERE reference IN :notification_references
         ORDER BY service_id, job_id"""
@@ -798,7 +798,7 @@ def get_letters_data_from_references(notification_references):
 
     with open('zips_sent_details.csv', 'w') as csvfile:
         csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(['notification_id', 'service_id', 'reference', 'job_id', 'created_at'])
+        csv_writer.writerow(['notification_id', 'service_id', 'template_id', 'reference', 'job_id', 'created_at'])
 
         for row in result:
             csv_writer.writerow(row)
