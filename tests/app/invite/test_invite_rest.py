8dcc65d11ce61d861b037b2a92416bf7a0e6cbc1
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2020-08-17 11:59:55 +0100

    Add endpoints to get invited users
    We want to display flash messages in admin when invites have been
    cancelled. This message needs to display the user's email address, so
    this commit adds endpoints to GET a single invited service user and org
    user so that we can look up the email address of a cancelled user.

--- tests/app/invite/test_invite_rest.py
+++ tests/app/invite/test_invite_rest.py
@@ -160,6 +160,29 @@ def test_get_invited_users_by_service_with_no_invites(client, notify_db, notify_
     assert len(json_resp['data']) == 0
 
 
+def test_get_invited_user_by_service(admin_request, sample_invited_user):
+    json_resp = admin_request.get(
+        'invite.get_invited_user_by_service',
+        service_id=sample_invited_user.service.id,
+        invited_user_id=sample_invited_user.id
+    )
+    assert json_resp['data']['email_address'] == sample_invited_user.email_address
+
+
+def test_get_invited_user_by_service_when_user_does_not_belong_to_the_service(
+    admin_request,
+    sample_invited_user,
+    fake_uuid,
+):
+    json_resp = admin_request.get(
+        'invite.get_invited_user_by_service',
+        service_id=fake_uuid,
+        invited_user_id=sample_invited_user.id,
+        _expected_status=404
+    )
+    assert json_resp['result'] == 'error'
+
+
 def test_update_invited_user_set_status_to_cancelled(client, sample_invited_user):
     data = {'status': 'cancelled'}
     url = '/service/{0}/invite/{1}'.format(sample_invited_user.service_id, sample_invited_user.id)
