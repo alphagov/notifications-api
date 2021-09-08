c9ea85938afc23b4de6752b9c6141185307c0c5c
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-09-08 11:16:46 +0100

    Refactor "create_user" to actually create a user
    This switches a number of fixtures to use "sample_user", which is
    equivalent to calling the previous "create_user" function when it
    used to default the email to "notify@...".

--- tests/app/test_commands.py
+++ tests/app/test_commands.py
@@ -1,5 +1,3 @@
-import uuid
-
 from app.commands import local_dev_broadcast_permissions
 from app.dao.services_dao import dao_add_user_to_service
 from tests.app.db import create_user
@@ -10,8 +8,7 @@ def test_local_dev_broadcast_permissions(
     sample_broadcast_service,
     notify_api,
 ):
-    # create_user will pull existing unless email is unique
-    user = create_user(email=f'{uuid.uuid4()}@example.com')
+    user = create_user()
     dao_add_user_to_service(sample_service, user)
     dao_add_user_to_service(sample_broadcast_service, user)
 
