b81a66da504e1ce11ed887336cd0b2dc7742e0da
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-12-13 16:48:30 +0000

    Fix assertions in tests for timeout DAO function
    Previously most of the assertions were being run *before* we had
    actually called the function. There was also a redundant block of
    assertions that just asserted the initial state of the test data.

--- tests/app/dao/notification_dao/test_notification_dao.py
+++ tests/app/dao/notification_dao/test_notification_dao.py
@@ -671,16 +671,13 @@ def test_dao_timeout_notifications(sample_template):
         pending = create_notification(sample_template, status='pending')
         delivered = create_notification(sample_template, status='delivered')
 
-    assert Notification.query.get(created.id).status == 'created'
-    assert Notification.query.get(sending.id).status == 'sending'
-    assert Notification.query.get(pending.id).status == 'pending'
-    assert Notification.query.get(delivered.id).status == 'delivered'
     temporary_failure_notifications = dao_timeout_notifications(1)
+
+    assert len(temporary_failure_notifications) == 2
     assert Notification.query.get(created.id).status == 'created'
     assert Notification.query.get(sending.id).status == 'temporary-failure'
     assert Notification.query.get(pending.id).status == 'temporary-failure'
     assert Notification.query.get(delivered.id).status == 'delivered'
-    assert len(temporary_failure_notifications) == 2
 
 
 def test_dao_timeout_notifications_only_updates_for_older_notifications(sample_template):
@@ -690,12 +687,13 @@ def test_dao_timeout_notifications_only_updates_for_older_notifications(sample_t
         pending = create_notification(sample_template, status='pending')
         delivered = create_notification(sample_template, status='delivered')
 
+    temporary_failure_notifications = dao_timeout_notifications(1)
+
+    assert len(temporary_failure_notifications) == 0
     assert Notification.query.get(created.id).status == 'created'
     assert Notification.query.get(sending.id).status == 'sending'
     assert Notification.query.get(pending.id).status == 'pending'
     assert Notification.query.get(delivered.id).status == 'delivered'
-    temporary_failure_notifications = dao_timeout_notifications(1)
-    assert len(temporary_failure_notifications) == 0
 
 
 def test_dao_timeout_notifications_doesnt_affect_letters(sample_letter_template):
@@ -705,12 +703,13 @@ def test_dao_timeout_notifications_doesnt_affect_letters(sample_letter_template)
         pending = create_notification(sample_letter_template, status='pending')
         delivered = create_notification(sample_letter_template, status='delivered')
 
+    temporary_failure_notifications = dao_timeout_notifications(1)
+
+    assert len(temporary_failure_notifications) == 0
     assert Notification.query.get(created.id).status == 'created'
     assert Notification.query.get(sending.id).status == 'sending'
     assert Notification.query.get(pending.id).status == 'pending'
     assert Notification.query.get(delivered.id).status == 'delivered'
-    temporary_failure_notifications = dao_timeout_notifications(1)
-    assert len(temporary_failure_notifications) == 0
 
 
 def test_should_return_notifications_excluding_jobs_by_default(sample_template, sample_job, sample_api_key):
