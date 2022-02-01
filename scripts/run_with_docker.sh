39848e6df0635c1cdd837c25fa0d055bd22e54f2
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2022-02-01 14:19:52 +0000

    move environment variables to their own lines and set -eu
    this means that if the environment variable can't be set (for example,
    if you don't have aws-cli installed) then there's a suitable error
    message early on.

--- scripts/run_with_docker.sh
+++ scripts/run_with_docker.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+set -eu
+
 DOCKER_IMAGE_NAME=notifications-api
 
 source environment.sh
 
+# this script should be run from within your virtualenv so you can access the aws cli
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-"$(aws configure get aws_access_key_id)"}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-"$(aws configure get aws_secret_access_key)"}
+: "${SQLALCHEMY_DATABASE_URI:=postgresql://postgres@host.docker.internal/notification_api}"
+
 docker run -it --rm \
-  -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-$(aws configure get aws_access_key_id)} \
-  -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-$(aws configure get aws_secret_access_key)} \
-  -e SQLALCHEMY_DATABASE_URI=${SQLALCHEMY_DATABASE_URI:-postgresql://postgres@host.docker.internal/notification_api} \
+  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  -e SQLALCHEMY_DATABASE_URI=$SQLALCHEMY_DATABASE_URI \
   -v $(pwd):/home/vcap/app \
-  ${DOCKER_ARGS} \
   ${DOCKER_IMAGE_NAME} \
   ${@}
