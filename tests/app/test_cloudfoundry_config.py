1f83113e748963cda9688d88a5d36dd7f9a54c1f
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2022-04-12 14:46:47 +0100

    Move setting VCAP_SERVICES out of fixture
    This was inconsistent with the source data for the fixture being
    overidden in some of the tests. We only need to set it in the env
    once, so it makes sense to just put the code there.

--- tests/app/test_cloudfoundry_config.py
+++ tests/app/test_cloudfoundry_config.py
@@ -22,12 +22,12 @@ def cloudfoundry_config():
 
 
 @pytest.fixture
-def cloudfoundry_environ(os_environ, cloudfoundry_config):
-    os.environ['VCAP_SERVICES'] = json.dumps(cloudfoundry_config)
+def vcap_application(os_environ):
     os.environ['VCAP_APPLICATION'] = '{"space_name": "🚀🌌"}'
 
 
-def test_extract_cloudfoundry_config_populates_other_vars(cloudfoundry_environ):
+def test_extract_cloudfoundry_config_populates_other_vars(cloudfoundry_config, vcap_application):
+    os.environ['VCAP_SERVICES'] = json.dumps(cloudfoundry_config)
     extract_cloudfoundry_config()
 
     assert os.environ['SQLALCHEMY_DATABASE_URI'] == 'postgresql uri'
@@ -35,7 +35,7 @@ def test_extract_cloudfoundry_config_populates_other_vars(cloudfoundry_environ):
     assert os.environ['NOTIFY_LOG_PATH'] == '/home/vcap/logs/app.log'
 
 
-def test_set_config_env_vars_ignores_unknown_configs(cloudfoundry_config, cloudfoundry_environ):
+def test_set_config_env_vars_ignores_unknown_configs(cloudfoundry_config, vcap_application):
     cloudfoundry_config['foo'] = {'credentials': {'foo': 'foo'}}
     cloudfoundry_config['user-provided'].append({
         'name': 'bar', 'credentials': {'bar': 'bar'}
