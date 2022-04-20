0457850fc0ea409451a90b6b078c33b48c5121b3
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2022-04-20 11:41:33 +0100

    Remove redundant conditional for CF Redis
    This is now used in all environments and we've removed support for
    non-CF Redis.

--- tests/app/test_cloudfoundry_config.py
+++ tests/app/test_cloudfoundry_config.py
@@ -29,11 +29,3 @@ def test_extract_cloudfoundry_config_populates_other_vars(os_environ, vcap_servi
 
     assert os.environ['SQLALCHEMY_DATABASE_URI'] == 'postgresql uri'
     assert os.environ['REDIS_URL'] == 'redis uri'
-
-
-def test_extract_cloudfoundry_config_copes_if_redis_not_set(os_environ, vcap_services):
-    del vcap_services['redis']
-    os.environ['VCAP_SERVICES'] = json.dumps(vcap_services)
-
-    extract_cloudfoundry_config()
-    assert 'REDIS_URL' not in os.environ
