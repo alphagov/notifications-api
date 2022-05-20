353ff5cea36461f8994a01971feb6afaa56f2947
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2022-05-20 14:07:12 +0100

    remove unused route
    was removed over a year ago
    https://github.com/alphagov/notifications-admin/commit/38cfee639084103bf2fca3dc8521b119e7c72430

--- app/platform_stats/rest.py
+++ app/platform_stats/rest.py
@@ -66,7 +66,6 @@ def validate_date_range_is_within_a_financial_year(start_date, end_date):
     return start_date, end_date
 
 
-@platform_stats_blueprint.route('usage-for-all-services')
 @platform_stats_blueprint.route('data-for-billing-report')
 def get_data_for_billing_report():
     start_date = request.args.get('start_date')
