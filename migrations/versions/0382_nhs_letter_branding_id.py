16e42ffb2acc837f6325a4bb060717f2b97f418f
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2022-11-15 11:21:22 +0000

    Give NHS letter branding the same ID in each environment
    We gave the NHS email branding the same ID in each environment, which
    simplifies the code to give it to organisations and to use it on forms.
    
    This does the same for the NHS letter branding ID. The logo exists in
    all environments. We delete the row in the letter_branding table from
    all environments apart from production, then re-insert it to the other
    environments with the ID that exists in production.

---
+++ migrations/versions/0382_nhs_letter_branding_id.py
@@ -0,0 +1,76 @@
+"""
+
+Revision ID: 0382_nhs_letter_branding_id
+Revises: 0381_letter_branding_to_org
+Create Date: 2022-11-15 07:57:49.060820
+
+"""
+import os
+
+from alembic import op
+
+revision = '0382_nhs_letter_branding_id'
+down_revision = '0381_letter_branding_to_org'
+
+environment = os.environ["NOTIFY_ENVIRONMENT"]
+
+
+def upgrade():
+    if environment not in ["live", "production"]:
+        op.execute(
+            """
+            DELETE FROM service_letter_branding
+            WHERE letter_branding_id in (
+                SELECT id
+                FROM letter_branding
+                WHERE name = 'NHS'
+            )
+        """
+        )
+
+        op.execute(
+            """
+            DELETE FROM letter_branding_to_organisation
+            WHERE letter_branding_id in (
+                SELECT id
+                FROM letter_branding
+                WHERE name = 'NHS'
+            )
+        """
+        )
+
+        op.execute(
+            """
+            UPDATE organisation SET letter_branding_id = null
+            WHERE letter_branding_id in(
+                SELECT id
+                FROM letter_branding
+                WHERE name = 'NHS'
+            )
+        """
+        )
+
+        op.execute(
+            """
+            DELETE FROM letter_branding WHERE name = 'NHS'
+        """
+        )
+
+        op.execute(
+            """
+            INSERT INTO letter_branding (
+                id, name, filename
+            )
+            VALUES (
+                '2cd354bb-6b85-eda3-c0ad-6b613150459f',
+                'NHS',
+                'nhs'
+            )
+        """
+        )
+
+
+def downgrade():
+    """
+    No downgrade step since this is not fully reversible, but won't be run in production.
+    """
