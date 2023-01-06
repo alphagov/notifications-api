631a7003fb9ba6e9fbe50eebf34a2c0389580b49
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2023-01-06 11:23:33 +0000

    Add new columns for letter branding
    This adds new columns to the letter branding table to record when a row
    was created or updated and who by. The email branding table uses
    slightly different column names (`created_by` and `updated_by` instead
    of `created_by_id` and `updated_by_id`). Adding `_id` on the end of the
    column names is more consistent with the other tables, so we can
    potentially change the email branding column names later.

---
+++ migrations/versions/0394_letter_branding_cols.py
@@ -0,0 +1,31 @@
+"""
+
+Revision ID: 0394_letter_branding_cols
+Revises: 0393_add_go_live_template
+Create Date: 2023-01-05 14:12:47.272639
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = "0394_letter_branding_cols"
+down_revision = "0393_add_go_live_template"
+
+
+def upgrade():
+    op.add_column("letter_branding", sa.Column("created_at", sa.DateTime(), nullable=True))
+    op.add_column("letter_branding", sa.Column("created_by_id", postgresql.UUID(as_uuid=True), nullable=True))
+    op.add_column("letter_branding", sa.Column("updated_at", sa.DateTime(), nullable=True))
+    op.add_column("letter_branding", sa.Column("updated_by_id", postgresql.UUID(as_uuid=True), nullable=True))
+    op.create_foreign_key("letter_branding_created_by_id_fkey", "letter_branding", "users", ["created_by_id"], ["id"])
+    op.create_foreign_key("letter_branding_updated_by_id_fkey", "letter_branding", "users", ["updated_by_id"], ["id"])
+
+
+def downgrade():
+    op.drop_constraint("letter_branding_created_by_id_fkey", "letter_branding", type_="foreignkey")
+    op.drop_constraint("letter_branding_updated_by_id_fkey", "letter_branding", type_="foreignkey")
+    op.drop_column("letter_branding", "updated_by_id")
+    op.drop_column("letter_branding", "updated_at")
+    op.drop_column("letter_branding", "created_by_id")
+    op.drop_column("letter_branding", "created_at")
