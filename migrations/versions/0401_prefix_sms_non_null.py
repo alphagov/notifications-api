8446eae21c7ecd413fae87aa80ff258e6adea58d
Samuel Williams <samuelhwilliams@gmail.com>
2023-01-19 17:04:03 +0000

    Make services_history.prefix_sms non-null
    This model is dynamically generated from the Service model, so it's not
    easy to have separate definitions for the columns on the main and
    history model.
    
    The main Service model has prefix_sms as non-null, which means they all
    have values. We can probably accept overwriting the historical
    prefix_sms nulls.

---
+++ migrations/versions/0401_prefix_sms_non_null.py
@@ -0,0 +1,21 @@
+"""
+
+Revision ID: 0401_prefix_sms_non_null
+Revises: 0400_non_nullable_ids
+Create Date: 2023-01-19 16:56:49.635251
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+revision = "0401_prefix_sms_non_null"
+down_revision = "0400_non_nullable_ids"
+
+
+def upgrade():
+    op.execute("UPDATE services_history SET prefix_sms = FALSE WHERE prefix_sms IS NULL")
+    op.alter_column("services_history", "prefix_sms", existing_type=sa.BOOLEAN(), nullable=False)
+
+
+def downgrade():
+    op.alter_column("services_history", "prefix_sms", existing_type=sa.BOOLEAN(), nullable=True)
