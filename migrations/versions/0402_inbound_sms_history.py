ba034027d484b4c66a794e457bf347fb73d5e021
Samuel Williams <samuelhwilliams@gmail.com>
2023-01-19 17:33:31 +0000

    Add missing index to history table

---
+++ migrations/versions/0402_inbound_sms_history.py
@@ -0,0 +1,19 @@
+"""
+
+Revision ID: 0402_inbound_sms_history
+Revises: 0401_prefix_sms_non_null
+Create Date: 2023-01-19 17:32:56.494917
+
+"""
+from alembic import op
+
+revision = "0402_inbound_sms_history"
+down_revision = "0401_prefix_sms_non_null"
+
+
+def upgrade():
+    op.create_index(op.f("ix_inbound_sms_history_created_at"), "inbound_sms_history", ["created_at"], unique=False)
+
+
+def downgrade():
+    op.drop_index(op.f("ix_inbound_sms_history_created_at"), table_name="inbound_sms_history")
