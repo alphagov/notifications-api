41f5909e75db021e8a4ca616529eee75ca56b65f
Samuel Williams <samuelhwilliams@gmail.com>
2023-01-19 16:14:07 +0000

    Make user_to_service fields non-nullable
    We have no records in this table where the values are
    NULL, so we can enforce this now.

---
+++ migrations/versions/0400_non_nullable_ids.py
@@ -0,0 +1,22 @@
+"""
+
+Revision ID: 0400_non_nullable_ids
+Revises: 0399_nullable_message_limit
+Create Date: 2023-01-19 16:13:38.601465
+
+"""
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+revision = "0400_non_nullable_ids"
+down_revision = "0399_nullable_message_limit"
+
+
+def upgrade():
+    op.alter_column("user_to_service", "user_id", existing_type=postgresql.UUID(), nullable=False)
+    op.alter_column("user_to_service", "service_id", existing_type=postgresql.UUID(), nullable=False)
+
+
+def downgrade():
+    op.alter_column("user_to_service", "service_id", existing_type=postgresql.UUID(), nullable=True)
+    op.alter_column("user_to_service", "user_id", existing_type=postgresql.UUID(), nullable=True)
