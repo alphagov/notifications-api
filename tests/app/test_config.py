4a41531d8da7d57591a6b728116b1ae1c935d3f9
Samuel Williams <samuelhwilliams@gmail.com>
2023-01-31 09:53:38 +0000

    Update test for remaining SQLAlchemy config

--- tests/app/test_config.py
+++ tests/app/test_config.py
@@ -85,6 +85,13 @@ def test_queue_names_all_queues_correct():
     ) == set(queues)
 
 
-def test_sql_statement_timeout(notify_db_session):
+def test_sqlalchemy_config(notify_api, notify_db_session):
     timeout = notify_db_session.execute("show statement_timeout").scalar()
     assert timeout == "20min"
+    assert notify_api.config["SQLALCHEMY_ENGINE_OPTIONS"]["connect_args"]["options"] == "-c statement_timeout=1200000"
+
+    from app import db
+
+    assert db.engine.pool.size() == notify_api.config["SQLALCHEMY_ENGINE_OPTIONS"]["pool_size"]
+    assert db.engine.pool.timeout() == notify_api.config["SQLALCHEMY_ENGINE_OPTIONS"]["pool_timeout"]
+    assert db.engine.pool._recycle == notify_api.config["SQLALCHEMY_ENGINE_OPTIONS"]["pool_recycle"]
