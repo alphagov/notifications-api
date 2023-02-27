e07e1971fa121056099be3909c98efd8f66d7a87
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2023-02-27 15:01:07 +0000

    Add new queue and worker for sending API letters
    This adds a new queue, `send-letter-tasks`, and worker
    `delivery-worker-sender-letters` that will be used for sending letters
    to the DVLA via the new API.

--- tests/app/test_config.py
+++ tests/app/test_config.py
@@ -60,7 +60,7 @@ def test_load_config_if_cloudfoundry_not_available(reload_config):
 def test_queue_names_all_queues_correct():
     # Need to ensure that all_queues() only returns queue names used in API
     queues = QueueNames.all_queues()
-    assert len(queues) == 18
+    assert len(queues) == 19
     assert set(
         [
             QueueNames.PRIORITY,
@@ -68,6 +68,7 @@ def test_queue_names_all_queues_correct():
             QueueNames.DATABASE,
             QueueNames.SEND_SMS,
             QueueNames.SEND_EMAIL,
+            QueueNames.SEND_LETTER,
             QueueNames.RESEARCH_MODE,
             QueueNames.REPORTING,
             QueueNames.JOBS,
