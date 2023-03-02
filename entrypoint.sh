00693c0fc24fea6e805b30a9726da42d813e0567
jonathan-ryding <jonathan.ryding@digital.cabinet-office.gov.uk>
2023-03-02 12:15:30 +0000

    Remove shifts in Bash script because they are not needed. The 'elif' logic flow makes them redundant.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,20 @@
 
 if [ "$1" == "worker" ]
 then
-  shift 1
   # TODO: This is fine for local development but this file will soon need to change to have
   # many different celery workers like we currently do in production
   celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency=4
 elif [ "$1" == "beat" ]
 then
-  shift 1
   celery -A run_celery.notify_celery beat --loglevel=INFO
 elif [ "$1" == "migration" ]
 then
-  shift 1
   flask db upgrade
 elif [ "$1" == "api" ]
 then
-  shift 1
   gunicorn -c /home/vcap/app/gunicorn_config.py application
 elif [ "$1" == "api-local" ]
 then
-  shift 1
   flask run --host 0.0.0.0 --port $PORT
 elif [ -n "$*" ]
 then
