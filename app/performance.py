fa0e4dd17e784d99eca59566bd00e49fad58a870
Samuel Williams <samuelhwilliams@gmail.com>
2023-04-03 13:19:08 +0100

    Remove NewRelic

--- app/performance.py
+++ app/performance.py
@@ -18,12 +18,6 @@ def init_performance_monitoring():
     enable_apm = environment in {"development", "preview", "staging"}
 
     if enable_apm:
-        if os.getenv("NEW_RELIC_ENABLED") == "1":
-            import newrelic.agent
-
-            # Expects NEW_RELIC_LICENSE_KEY set in environment as well.
-            newrelic.agent.initialize("newrelic.ini", environment=environment, ignore_errors=False)
-
         if sentry_dsn := os.getenv("SENTRY_DSN"):
             import sentry_sdk
 
