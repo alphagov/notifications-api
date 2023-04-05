12c4816f6e102e8f15a8ceceeaa5fb077d4d9cc3
Samuel Williams <samuelhwilliams@gmail.com>
2023-04-05 14:23:16 +0100

    Enable for a single instance in produdction

--- app/performance.py
+++ app/performance.py
@@ -13,19 +13,22 @@ def init_performance_monitoring():
     environment = os.getenv("NOTIFY_ENVIRONMENT").lower()
     enable_apm = environment in {"development", "preview", "staging"}
 
-    if enable_apm:
-        if sentry_dsn := os.getenv("SENTRY_DSN"):
-            import sentry_sdk
+    if environment == "production" and os.getenv("CF_INSTANCE_INDEX", "-1") == "9":
+        # In production, turn it on for instance 9 (randomly chosen) only.
+        enable_apm = True
 
-            sentry_sdk.init(
-                dsn=sentry_dsn,
-                environment=environment,
-                debug=False,
-                sample_rate=float(os.getenv("SENTRY_SAMPLE_RATE", 1.0)),  # Error sampling rate
-                attach_stacktrace=False,  # Attach stacktraces to _all_ events (ie even log messages)
-                send_default_pii=False,  # Don't include any default PII (false by default, here for explicitness)
-                request_bodies="never",  # Include request body (eg POST payload) in sentry errors
-                traces_sampler=partial(
-                    sentry_sampler, sample_rate=float(os.getenv("SENTRY_TRACING_SAMPLE_RATE", 0))
-                ),  # Custom decision-maker for sampling traces
-            )
+    if enable_apm and (sentry_dsn := os.getenv("SENTRY_DSN")):
+        import sentry_sdk
+
+        sentry_sdk.init(
+            dsn=sentry_dsn,
+            environment=environment,
+            debug=False,
+            sample_rate=float(os.getenv("SENTRY_SAMPLE_RATE", 1.0)),  # Error sampling rate
+            attach_stacktrace=False,  # Attach stacktraces to _all_ events (ie even log messages)
+            send_default_pii=False,  # Don't include any default PII (false by default, here for explicitness)
+            request_bodies="never",  # Include request body (eg POST payload) in sentry errors
+            traces_sampler=partial(
+                sentry_sampler, sample_rate=float(os.getenv("SENTRY_TRACING_SAMPLE_RATE", 0))
+            ),  # Custom decision-maker for sampling traces
+        )
