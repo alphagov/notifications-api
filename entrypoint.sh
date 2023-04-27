34056a6417806ea236c8f0a1dbc9f20222d76fe1
Roberto Gaidarji <roberto.gaidarji@digital.cabinet-office.gov.uk>
2023-04-27 13:47:14 +0100

    Added queues to entrypoint.sh 	modified:   entrypoint.sh Modified entrypoint.sh to cater for all queues

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,77 @@
 
 if [ "$1" == "worker" ]
 then
-  # TODO: This is fine for local development but this file will soon need to change to have
-  # many different celery workers like we currently do in production
-  celery -A run_celery.notify_celery worker --pidfile="/tmp/celery.pid" --loglevel=INFO --concurrency=4
+  #!/bin/bash
+case $NOTIFY_APP_NAME in
+  api)
+    unset GUNICORN_CMD_ARGS
+    exec scripts/run_app_paas.sh gunicorn -c /home/vcap/app/gunicorn_config.py application
+    ;;
+  delivery-worker-retry-tasks)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=4 \
+    -Q retry-tasks 2> /dev/null
+    ;;
+  delivery-worker-letters)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=4 \
+    -Q create-letters-pdf-tasks,letter-tasks 2> /dev/null
+    ;;
+  delivery-worker-jobs)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=4 \
+    -Q database-tasks,job-tasks 2> /dev/null
+    ;;
+  delivery-worker-research)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=4 \
+    -Q research-mode-tasks 2> /dev/null
+    ;;
+  delivery-worker-sender)
+    exec scripts/run_multi_worker_app_paas.sh celery multi start 3 -c 4 -A run_celery.notify_celery --loglevel=INFO \
+    --logfile=/dev/null --pidfile=/tmp/celery%N.pid -Q send-sms-tasks,send-email-tasks
+    ;;
+  delivery-worker-sender-letters)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=4 \
+    -Q send-letter-tasks 2> /dev/null
+    ;;
+  delivery-worker-periodic)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=2 \
+    -Q periodic-tasks 2> /dev/null
+    ;;
+  delivery-worker-reporting)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=4 \
+    -Q reporting-tasks 2> /dev/null
+    ;;
+  delivery-worker-priority)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=4 \
+    -Q priority-tasks 2> /dev/null
+    ;;
+  # Only consume the notify-internal-tasks queue on this app so that Notify messages are processed as a priority
+  delivery-worker-internal)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=4 \
+    -Q notify-internal-tasks 2> /dev/null
+    ;;
+  delivery-worker-broadcasts)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=2 \
+    -Q broadcast-tasks 2> /dev/null
+    ;;
+  delivery-worker-receipts)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=4 \
+    -Q ses-callbacks,sms-callbacks 2> /dev/null
+    ;;
+  delivery-worker-service-callbacks)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=4 \
+    -Q service-callbacks,service-callbacks-retry 2> /dev/null
+    ;;
+  delivery-worker-save-api-notifications)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery worker --loglevel=INFO --concurrency=4 \
+    -Q save-api-email-tasks,save-api-sms-tasks 2> /dev/null
+    ;;
+  delivery-celery-beat)
+    exec scripts/run_app_paas.sh celery -A run_celery.notify_celery beat --loglevel=INFO
+    ;;
+  *)
+    echo "Unknown notify_app_name $NOTIFY_APP_NAME"
+    exit 1
+    ;;
+esac
 elif [ "$1" == "beat" ]
 then
   celery -A run_celery.notify_celery beat --loglevel=INFO
