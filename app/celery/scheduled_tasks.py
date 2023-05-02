967c02962a01044cab86fbe44747ffe89b32521e
Samuel Williams <samuelhwilliams@gmail.com>
2023-05-02 08:00:04 +0100

    Add cronitor check

--- app/celery/scheduled_tasks.py
+++ app/celery/scheduled_tasks.py
@@ -596,6 +596,7 @@ def populate_annual_billing(year, missing_services_only):
 
 
 @notify_celery.task(name="run-populate-annual-billing")
+@cronitor("run-populate-annual-billing")
 def run_populate_annual_billing():
     year = get_current_financial_year_start_year()
     run_populate_annual_billing(year=year, missing_services_only=True)
