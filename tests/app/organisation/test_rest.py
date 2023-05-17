1da590a65922bf902a0880889e5ee584f7af2d56
Samuel Williams <samuelhwilliams@gmail.com>
2023-05-17 15:33:25 +0100

    Rename org search tests

--- tests/app/organisation/test_rest.py
+++ tests/app/organisation/test_rest.py
@@ -167,7 +167,7 @@ def test_get_organisation_by_domain(admin_request, notify_db_session, domain, ex
         assert response["result"] == "error"
 
 
-def test_find_organisations_by_name_finds_organisations(notify_db_session, admin_request):
+def test_organisation_search_finds_organisations(notify_db_session, admin_request):
     organisation_1 = create_organisation(name="ABCDEF")
     organisation_2 = create_organisation(name="ABCGHT")
     create_organisation(name="ZYSWVU")
@@ -175,13 +175,13 @@ def test_find_organisations_by_name_finds_organisations(notify_db_session, admin
     assert response["data"] == [organisation_1.serialize_for_list(), organisation_2.serialize_for_list()]
 
 
-def test_find_organisations_by_name_handles_no_results(notify_db_session, admin_request):
+def test_organisation_search_handles_no_results(notify_db_session, admin_request):
     create_organisation(name="ABCDEF")
     response = admin_request.get("organisation.search", name="1234")
     assert response["data"] == []
 
 
-def test_find_organisations_by_name_handles_special_characters(notify_db_session, admin_request):
+def test_organisation_search_handles_special_characters(notify_db_session, admin_request):
     create_organisation(name="ABCDEF")
     organisation_2 = create_organisation(name="ZYX % WVU")
     create_organisation(name="123456")
@@ -189,7 +189,7 @@ def test_find_organisations_by_name_handles_special_characters(notify_db_session
     assert response["data"] == [organisation_2.serialize_for_list()]
 
 
-def test_find_organisations_by_name_handles_no_organisation_name(notify_db_session, admin_request):
+def test_organisation_search_handles_no_organisation_name(notify_db_session, admin_request):
     admin_request.get("organisation.search", _expected_status=400)
 
 
