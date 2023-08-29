87ac9197614a9ef3b57d251fae5eec7ac13095f1
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2023-08-29 18:45:21 +0100

    migration to add normalised_service_name column
    this is the first in a process to rename `email_from` to
    `normalised_service_name`.
    
    1. * add new column as nullable *
    2. write new values to both new and old column
    3. migrate old data. add unique and not null constraints.
    4. delete references to old column in code
    5. drop old column

---
+++ migrations/versions/0420_unique_service_name_1.py
@@ -0,0 +1,23 @@
+"""
+
+Revision ID: 0420_unique_service_name_1
+Revises: 0419_take_part_in_research
+Create Date: 2023-08-24 16:26:03.488048
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = "0420_unique_service_name_1"
+down_revision = "0419_take_part_in_research"
+
+
+def upgrade():
+    op.add_column("services", sa.Column("normalised_service_name", sa.String(), nullable=True, unique=True))
+    op.add_column("services_history", sa.Column("normalised_service_name", sa.String(), nullable=True))
+
+
+def downgrade():
+    op.drop_column("services", "normalised_service_name")
+    op.drop_column("services_history", "normalised_service_name")
