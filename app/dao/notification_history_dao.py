492d802341674a91f6e98a149cf1d44883af0b84
David McDonald <david.mcdonald@digital.cabinet-office.gov.uk>
2023-09-01 15:24:02 +0100

    Use created_at to delete old notification_history, not sent_at
    As per conversation in
    https://github.com/alphagov/notifications-api/pull/3860#discussion_r1309733204
    
    It makes more sense to have consistency with how we do billing
    which is generally (for sms and emails) by looking at the created_at
    rather than the sent_at.

--- app/dao/notification_history_dao.py
+++ app/dao/notification_history_dao.py
@@ -9,7 +9,7 @@ def delete_notification_history_older_than_datetime(older_than_datetime):
     current_app.logger.info("Beginning to delete notification_history older than %s", older_than_datetime)
 
     num_rows_deleted = NotificationHistory.query.filter(
-        NotificationHistory.sent_at < older_than_datetime,
+        NotificationHistory.created_at < older_than_datetime,
     ).delete()
 
     current_app.logger.info(
