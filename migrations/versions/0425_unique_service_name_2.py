888561c7d1d63375331cd13d91665df14517a091
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2023-09-11 14:55:53 +0100

    migrate email_from values and add unique/not-null constraint

---
+++ migrations/versions/0425_unique_service_name_2.py
@@ -0,0 +1,37 @@
+"""
+
+Revision ID: 0425_unique_service_name_2
+Revises: 0424_n_history_created_at
+Create Date: 2023-09-01 10:10:19.011060
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = "0425_unique_service_name_2"
+down_revision = "0424_n_history_created_at"
+
+
+def upgrade():
+    op.execute("UPDATE services SET normalised_service_name = email_from WHERE normalised_service_name IS NULL")
+    op.execute("UPDATE services_history SET normalised_service_name = email_from WHERE normalised_service_name IS NULL")
+    op.alter_column("services", "normalised_service_name", existing_type=sa.VARCHAR(), nullable=False)
+    op.alter_column("services_history", "normalised_service_name", existing_type=sa.VARCHAR(), nullable=False)
+    op.alter_column("services", "email_from", existing_type=sa.VARCHAR(), nullable=True)
+    op.alter_column("services_history", "email_from", existing_type=sa.VARCHAR(), nullable=True)
+    op.drop_constraint("services_email_from_key", "services", type_="unique")
+
+
+def downgrade():
+    op.alter_column("services_history", "normalised_service_name", existing_type=sa.VARCHAR(), nullable=True)
+    op.alter_column("services", "normalised_service_name", existing_type=sa.VARCHAR(), nullable=True)
+
+    # back-fill any null email_from from normalised
+    op.execute("UPDATE services SET email_from = normalised_service_name")
+    op.execute("UPDATE services_history SET email_from = normalised_service_name")
+
+    # now we can restore null constraint and unique index
+    op.alter_column("services", "email_from", existing_type=sa.VARCHAR(), nullable=True)
+    op.alter_column("services_history", "email_from", existing_type=sa.VARCHAR(), nullable=True)
+    op.create_unique_constraint("services_email_from_key", "services", columns=["email_from"])
