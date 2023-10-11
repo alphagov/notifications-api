4fd50207550ce4ac130baeb3ff621eae9cf5e68d
David McDonald <david.mcdonald@digital.cabinet-office.gov.uk>
2023-10-11 12:25:13 +0100

    Use tablesample on query to get random notifications
    As per the inline code comment, we use tablesample to get our
    workers picking random notifications to delete.
    
    This was taken from
    https://stackoverflow.com/questions/8674718/best-way-to-select-random-rows-postgresql
    
    I've also manually run
    `select id from notification_history TABLESAMPLE SYSTEM (0.005) LIMIT 50000;`
    on production and found it returning results in under 5 seconds which
    leads me to think it is performant enough and won't actually slow
    things down too much.

--- app/dao/notification_history_dao.py
+++ app/dao/notification_history_dao.py
@@ -6,13 +6,27 @@ from app import db
 def delete_notification_history_older_than_datetime(older_than_datetime, query_limit=50000):
     current_app.logger.info("Beginning to delete notification_history older than %s", older_than_datetime)
 
+    # We want to run this function concurrently, ie having several workers running this same task at the same time
+    # To ensure workers don't try and delete the same notifications (which will cause them to stop and exit)
+    # we get them to pick a random sample of notifications
+    # In production, to get 50,000 results we only need a random sample of about 0.05% of the table to give us
+    # 50,000 rows
+    # However, in other environments, including test, we don't have many notifications in the notification_history
+    # table and therefore need to get all of them (and don't need to worry about performance)
+    if current_app.config["NOTIFY_ENVIRONMENT"] == "production":
+        tablesample_percentage = 0.05
+    else:
+        tablesample_percentage = 100
+
     total_deleted = 0
     deleted_this_iteration = 1
     while deleted_this_iteration > 0:
         delete_query = f"""
             DELETE FROM notification_history
             where id in (
-                select id from notification_history where created_at < '{older_than_datetime}' limit {query_limit}
+                select id from notification_history TABLESAMPLE SYSTEM ({tablesample_percentage})
+                where created_at < '{older_than_datetime}'
+                limit {query_limit}
             )
         """
 
