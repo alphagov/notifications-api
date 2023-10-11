5a1fa32849d550babbd0ad0bcd0f5fb5e19887eb
David McDonald <david.mcdonald@digital.cabinet-office.gov.uk>
2023-10-11 12:15:26 +0100

    Swap to using explicit query string rather than models
    There is no functionality/behaviour change here. This is a refactor
    that means we can write the sql explicitely.
    
    This will come in useful for the next commit where we start to use
    `TABLESAMPLE SYSTEM` in our SQL which I was struggling to get to
    work using the models.

--- app/dao/notification_history_dao.py
+++ app/dao/notification_history_dao.py
@@ -1,8 +1,6 @@
 from flask import current_app
-from sqlalchemy import select
 
 from app import db
-from app.models import NotificationHistory
 
 
 def delete_notification_history_older_than_datetime(older_than_datetime, query_limit=50000):
@@ -11,24 +9,19 @@ def delete_notification_history_older_than_datetime(older_than_datetime, query_l
     total_deleted = 0
     deleted_this_iteration = 1
     while deleted_this_iteration > 0:
-        notification_ids_to_delete = (
-            select(NotificationHistory.id)
-            .where(NotificationHistory.created_at < older_than_datetime)
-            .limit(query_limit)
-        )
+        delete_query = f"""
+            DELETE FROM notification_history
+            where id in (
+                select id from notification_history where created_at < '{older_than_datetime}' limit {query_limit}
+            )
+        """
 
-        current_app.logger.info("Found chunk of rows from notification_history to delete")
-
-        num_rows_deleted = NotificationHistory.query.filter(
-            NotificationHistory.id.in_(notification_ids_to_delete),
-        ).delete(synchronize_session=False)
-
-        current_app.logger.info("Deleted chunk of %s rows from notification_history", num_rows_deleted)
-
+        num_rows_deleted = db.session.execute(delete_query).rowcount
+        db.session.commit()
         deleted_this_iteration = num_rows_deleted
         total_deleted += num_rows_deleted
 
-        db.session.commit()
+        current_app.logger.info("Deleted chunk of %s rows from notification_history", num_rows_deleted)
 
     current_app.logger.info(
         "Deleted %s rows from notification_history older than %s", total_deleted, older_than_datetime
