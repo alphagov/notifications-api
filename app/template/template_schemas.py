c73d257a4baa7e03af75d863abcea549eceeaba1
Samuel Williams <samuelhwilliams@gmail.com>
2023-12-12 12:53:41 +0000

    Allow creating letters with bilingual fields
    When we added support for bilingual letters, we didn't allow creating
    them (via the admin API) from scratch. You would need to create a basic
    letter template, then POST an update with bilingual fields.
    
    In order to allow efficient copying of templates, let's make it possible
    to create a letter template with bilingual fields in a single request.

--- app/template/template_schemas.py
+++ app/template/template_schemas.py
@@ -1,45 +1,8 @@
 from app.constants import TEMPLATE_PROCESS_TYPE, TEMPLATE_TYPES, LetterLanguageOptions
 from app.schema_validation.definitions import nullable_uuid, uuid
 
-post_create_template_schema = {
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "description": "POST create new template",
-    "type": "object",
-    "title": "payload for POST /service/<uuid:service_id>/template",
+letter_languages_subschema = {
     "properties": {
-        "name": {"type": "string"},
-        "template_type": {"enum": TEMPLATE_TYPES},
-        "service": uuid,
-        "process_type": {"enum": TEMPLATE_PROCESS_TYPE},
-        "content": {"type": "string"},
-        "subject": {"type": "string"},
-        "created_by": uuid,
-        "parent_folder_id": uuid,
-        "postage": {"type": "string", "format": "postage"},
-    },
-    "if": {"properties": {"template_type": {"enum": ["email", "letter"]}}},
-    "then": {"required": ["subject"]},
-    "required": ["name", "template_type", "content", "service", "created_by"],
-}
-
-post_update_template_schema = {
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "description": "POST update existing template",
-    "type": "object",
-    "title": "payload for POST /service/<uuid:service_id>/template/<uuid:template_id>",
-    "properties": {
-        "id": uuid,
-        "name": {"type": "string"},
-        "template_type": {"enum": TEMPLATE_TYPES},
-        "service": uuid,
-        "process_type": {"enum": TEMPLATE_PROCESS_TYPE},
-        "content": {"type": "string"},
-        "subject": {"type": "string"},
-        "postage": {"type": "string", "format": "postage"},
-        "reply_to": nullable_uuid,
-        "created_by": uuid,
-        "archived": {"type": "boolean"},
-        "current_user": uuid,
         "letter_languages": {"type": "string", "enum": [i.value for i in LetterLanguageOptions]},
     },
     # read as: "if `letter_languages` is present
@@ -84,4 +47,52 @@ post_update_template_schema = {
             },
         ]
     },
+}
+
+post_create_template_schema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "description": "POST create new template",
+    "type": "object",
+    "title": "payload for POST /service/<uuid:service_id>/template",
+    "properties": {
+        "name": {"type": "string"},
+        "template_type": {"enum": TEMPLATE_TYPES},
+        "service": uuid,
+        "process_type": {"enum": TEMPLATE_PROCESS_TYPE},
+        "content": {"type": "string"},
+        "subject": {"type": "string"},
+        "created_by": uuid,
+        "parent_folder_id": uuid,
+        "postage": {"type": "string", "format": "postage"},
+    },
+    "allOf": [
+        {
+            "if": {"properties": {"template_type": {"enum": ["email", "letter"]}}},
+            "then": {"required": ["subject"]},
+            "required": ["name", "template_type", "content", "service", "created_by"],
+        },
+        letter_languages_subschema,
+    ],
+}
+
+post_update_template_schema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "description": "POST update existing template",
+    "type": "object",
+    "title": "payload for POST /service/<uuid:service_id>/template/<uuid:template_id>",
+    "properties": {
+        "id": uuid,
+        "name": {"type": "string"},
+        "template_type": {"enum": TEMPLATE_TYPES},
+        "service": uuid,
+        "process_type": {"enum": TEMPLATE_PROCESS_TYPE},
+        "content": {"type": "string"},
+        "subject": {"type": "string"},
+        "postage": {"type": "string", "format": "postage"},
+        "reply_to": nullable_uuid,
+        "created_by": uuid,
+        "archived": {"type": "boolean"},
+        "current_user": uuid,
+    },
+    "allOf": [letter_languages_subschema],
 }
