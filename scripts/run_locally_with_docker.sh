a94edcc25cb1017275fc08dd7bea79d2d4ec5f26
Roberto Gaidarji <roberto.gaidarji@digital.cabinet-office.gov.uk>
2024-01-02 17:41:33 +0000

    Remove SQLALCHEMY env var
    This var is set within the "concourse_tests" layer of the Dockerfile.
    it is used to set connection parameters for postgresql.
    
    I have also removed it from the docker run command, where I also added the "-d" parameter to the run command to detach it and make the tests run available as output.

--- scripts/run_locally_with_docker.sh
+++ scripts/run_locally_with_docker.sh
@@ -8,7 +8,6 @@ source environment.sh
 # this script should be run from within your virtualenv so you can access the aws cli
 AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-"$(aws configure get aws_access_key_id)"}
 AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-"$(aws configure get aws_secret_access_key)"}
-SQLALCHEMY_DATABASE_URI=${SQLALCHEMY_DATABASE_URI:-"postgresql://postgres@host.docker.internal/notification_api"}
 REDIS_URL=${REDIS_URL:-"redis://host.docker.internal:6379"}
 API_HOST_NAME=${API_HOST_NAME:-"http://host.docker.internal:6011"}
 API_HOST_NAME_INTERNAL=${API_HOST_NAME_INTERNAL:-"http://host.docker.internal:6011"}
@@ -21,10 +20,9 @@ else
   EXPOSED_PORTS=""
 fi
 
-docker run -it --rm \
+docker run -it -d --name notifications_api --rm \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  -e SQLALCHEMY_DATABASE_URI=$SQLALCHEMY_DATABASE_URI \
   -e REDIS_ENABLED=${REDIS_ENABLED:-0} \
   -e REDIS_URL=$REDIS_URL \
   -e API_HOST_NAME=$API_HOST_NAME \
