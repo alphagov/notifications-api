8e7b340c3b47ac14a1d91857ec3e8562dcdba4f9
c4seyla <2852383+c4seyla@users.noreply.github.com>
2024-03-15 12:33:34 +0000

    date and datetime

--- app/dao/notifications_dao.py
+++ app/dao/notifications_dao.py
@@ -87,16 +87,17 @@ FIELDS_TO_TRANSFER_TO_NOTIFICATION_HISTORY = [
 
 
 def dao_get_last_date_template_was_used(template_id, service_id):
-    notification_query = db.session.query(db.func.date(Notification.created_at).label("date")).filter(
+    notification_query = db.session.query(Notification.created_at.label("date")).filter(
         Notification.service_id == service_id,
         Notification.template_id == template_id,
         Notification.key_type != KEY_TYPE_TEST,
     )
 
-    fact_notification_status_query = db.session.query(FactNotificationStatus.bst_date.label("date")).filter(
-        FactNotificationStatus.template_id == template_id, FactNotificationStatus.key_type != KEY_TYPE_TEST
-    )
+    fact_notification_status_query = db.session.query(
+        db.func.cast(FactNotificationStatus.bst_date, db.DateTime).label("date")
+    ).filter(FactNotificationStatus.template_id == template_id, FactNotificationStatus.key_type != KEY_TYPE_TEST)
 
+    # Combine, order, and fetch the latest date
     last_date_query = notification_query.union_all(fact_notification_status_query).order_by(db.desc("date")).limit(1)
     last_date_result = last_date_query.scalar()
 
