82c1a1674eac1023b84545f93d9be6201ff51865
c4seyla <2852383+c4seyla@users.noreply.github.com>
2024-03-15 17:31:16 +0000

    date returned all the time

--- app/dao/notifications_dao.py
+++ app/dao/notifications_dao.py
@@ -14,7 +14,7 @@ from notifications_utils.recipients import (
     validate_and_format_email_address,
 )
 from notifications_utils.timezones import convert_bst_to_utc, convert_utc_to_bst
-from sqlalchemy import and_, asc, desc, func, literal, or_, union
+from sqlalchemy import Date, and_, asc, desc, func, literal, or_, union
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.exc import NoResultFound
@@ -87,17 +87,20 @@ FIELDS_TO_TRANSFER_TO_NOTIFICATION_HISTORY = [
 
 
 def dao_get_last_date_template_was_used(template_id, service_id):
-    last_date = (
+    last_date_result = (
         db.session.query(
             functions.coalesce(
-                functions.max(Notification.created_at),
-                (
-                    db.session.query(functions.max(FactNotificationStatus.bst_date))
-                    .filter(
-                        FactNotificationStatus.template_id == template_id,
-                        FactNotificationStatus.key_type != KEY_TYPE_TEST,
-                    )
-                    .scalar_subquery()
+                functions.cast(functions.max(Notification.created_at), Date),
+                functions.cast(
+                    (
+                        db.session.query(functions.max(FactNotificationStatus.bst_date))
+                        .filter(
+                            FactNotificationStatus.template_id == template_id,
+                            FactNotificationStatus.key_type != KEY_TYPE_TEST,
+                        )
+                        .scalar_subquery()
+                    ),
+                    Date,
                 ),
             )
         )
@@ -109,7 +112,8 @@ def dao_get_last_date_template_was_used(template_id, service_id):
         .scalar()
     )
 
-    return last_date
+    # Ensure that last_date_result is a date object
+    return last_date_result.date() if isinstance(last_date_result, datetime) else last_date_result
 
 
 @autocommit
