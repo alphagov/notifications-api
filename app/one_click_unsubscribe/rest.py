b67c25b4bcee2c720f25bb30ed39e4287cad7a7c
Chukwugozie Mbeledogu <chukwugozie.mbeledogu@digital.cabinet-office.gov.uk>
2024-06-06 14:34:30 +0100

    update one_click_unsubscribe to use create_unsubscribe_request_dao

--- app/one_click_unsubscribe/rest.py
+++ app/one_click_unsubscribe/rest.py
@@ -3,6 +3,7 @@ from itsdangerous import BadData
 from notifications_utils.url_safe_token import check_token, generate_token
 
 from app.dao.notifications_dao import get_notification_by_id
+from app.dao.unsubscribe_request_dao import create_unsubscribe_request_dao
 from app.errors import InvalidRequest, register_errors
 
 one_click_unsubscribe_blueprint = Blueprint("one_click_unsubscribe", __name__)
@@ -15,28 +16,15 @@ def one_click_unsubscribe(notification_id, token):
     max_age_seconds = 60 * 60 * 24 * 365  # set to 1 year for now
 
     try:
-        email_address = check_token(
-            token, current_app.config["SECRET_KEY"], current_app.config["DANGEROUS_SALT"], max_age_seconds
-        )
+        check_token(token, current_app.config["SECRET_KEY"], current_app.config["DANGEROUS_SALT"], max_age_seconds)
 
     except BadData as e:
         errors = {"unsubscribe request": "This is not a valid unsubscribe link."}
         raise InvalidRequest(errors, status_code=400) from e
 
-    # Get data to populate unsubscribe request from the notification object
+    # Create unsubscribe request from the notification object
     notification = get_notification_by_id(notification_id)
-    unsubscribe_data = {  # noqa
-        "notification_id": notification_id,
-        "template_id": notification.template_id,
-        "template_version": notification.template_version,
-        "service_id": notification.service_id,
-        "email_address": email_address,
-    }
-
-    # TODO
-    # Once the migration for the new unsubscribe request table has been effected, the following lines of code can be
-    # used to add new unsubscribe request:
-    # unsubscribe_request = UnsubscribeRequest(**unsubscribe_data)
+    create_unsubscribe_request_dao(notification)
 
     current_app.logger.debug("Received unsubscribe request for notification_id: %s", notification_id)
 
