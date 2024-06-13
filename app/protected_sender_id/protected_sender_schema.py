7f22040fb65364503dbbe9d11468f3e378afbc19
Will Pearson <will.pearson@digital.cabinet-office.gov.uk>
2024-06-13 20:22:38 +0100

    Adding model and protected_sender_id/check endpoint
    This adds the ProtectedSenderID model and the check endpoint

---
+++ app/protected_sender_id/protected_sender_schema.py
@@ -0,0 +1,9 @@
+protected_sender_request = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "description": "protected sender id request schema",
+    "type": "object",
+    "title": "Protected sender id check",
+    "properties": {
+        "sender_id": {"type": ["string", "null"]},
+    },
+}
