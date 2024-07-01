5450fdf3d4fba1a393b50cfe2377094bc36dd5e8
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2024-07-01 11:29:04 +0100

    failing request on test

--- app/unsubscribe_requests/rest.py
+++ app/unsubscribe_requests/rest.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, current_app, jsonify
+from datetime import datetime
 
 
 
 unsubscrube_requests_blueprint = Blueprint("unsubscribe_requests", __name__)
 
-@unsubscrube_requests_blueprint.route("/unsubscribe/<uuid:notification_id>", methods=["GET"])
-def unsubscribe_requests(notification_id):
-    """
-    Get all unsubscribe requests, do we want a count as a query string?
-    """
-    pass
+@unsubscrube_requests_blueprint.route("/unsubscribe/<uuid:notification_id>/summary", methods=["GET"])
+def unsubscribe_requests_summary(notification_id):
+    pending_unsubscribe_requests = 250
+    datetime_of_latest_unsubscribe_request = datetime.now()
+    return jsonify(pending_unsubscribe_requests=pending_unsubscribe_requests, datetime_of_latest_unsubscribe_request=datetime_of_latest_unsubscribe_request), 200
 
