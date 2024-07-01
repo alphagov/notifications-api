8dc16d00201a4e772665e914a6618407e8077443
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2024-07-01 13:55:55 +0100

    skeleton of tests using admin_request wrapper

--- tests/app/unsubscribe_requests/test_unsubscribe_requests.py
+++ tests/app/unsubscribe_requests/test_unsubscribe_requests.py
@@ -1,10 +1,4 @@
-
-def unsubscribe_summary_url_get(admin_request, notification_id):
-    return admin_request.get(f"/unsubscribe/{notification_id}/summary")
-
-def test_unsubscribe_request_returns_correct_number_of_pending_requests(client, sample_email_notification):
+def test_unsubscribe_request_returns_correct_number_of_pending_requests(admin_request, sample_email_notification):
     pending_unsubscribe_requests = 250
-    response = unsubscribe_summary_url_get(client, 12345)
-    response_json_data = response.get_json()
-    assert response.status_code == 200
-    assert response_json_data["pending_unsubscribe_requests"] == pending_unsubscribe_requests
+    response = admin_request.get("unsubscribe_requests.unsubscribe_requests_summary", notification_id=sample_email_notification.id)
+    assert response["pending_unsubscribe_requests"] == pending_unsubscribe_requests
