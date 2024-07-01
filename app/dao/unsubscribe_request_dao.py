dc74d9efa533780f0693c830bcd384e56905ceba
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2024-07-01 21:47:09 +0100

    failing test without db

--- app/dao/unsubscribe_request_dao.py
+++ app/dao/unsubscribe_request_dao.py
@@ -2,7 +2,7 @@ from sqlalchemy import desc
 
 from app import db
 from app.dao.dao_utils import autocommit
-from app.models import UnsubscribeRequest, UnsubscribeRequestReport
+from app.models import UnsubscribeRequest, Service
 
 
 @autocommit
@@ -15,23 +15,17 @@ def get_unsubscribe_request_by_notification_id_dao(notification_id):
 
 
 def get_count_of_unprocessed_requests(notification_id):
-    return len(
+    return (
         db.session.query(
             UnsubscribeRequest.id.label("unsubscribe_request_id"),
-            UnsubscribeRequestReport.processed_by_service_at.label(
-                "unsubscribe_request_report_processed_by_service_at"
-            ),
         )
         .select_from(UnsubscribeRequest)
         .join(
-            UnsubscribeRequestReport,
-            UnsubscribeRequestReport.id
-            == UnsubscribeRequest.unsubscribe_request_report_id,
+            Service,
+            Service.id == UnsubscribeRequest.service_id
         )
-        .filter(
-            UnsubscribeRequestReport.processed_by_service_at == True,
-            UnsubscribeRequest.notification_id == notification_id,
-        )
+        .filter(UnsubscribeRequest.id == notification_id)
+        .count()
     )
 
 
