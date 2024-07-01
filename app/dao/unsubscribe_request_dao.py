a7d659edf62a2450fd2287a6040f3e891bfd904d
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2024-07-01 14:11:29 +0100

    started count methods

--- app/dao/unsubscribe_request_dao.py
+++ app/dao/unsubscribe_request_dao.py
@@ -14,6 +14,18 @@ def get_unsubscribe_request_by_notification_id_dao(notification_id):
     return UnsubscribeRequest.query.filter_by(notification_id=notification_id).one()
 
 
+def get_count_of_unprocessed_requests(notification_id):
+    return (
+        db.session.query(UnsubscribeRequest.id.label("unsubscribe_request_id"))
+        .select_from(UnsubscribeRequest)
+        .join(
+            UnsubscribeRequestReport,
+            UnsubscribeRequestReport.id
+            == UnsubscribeRequest.unsubscribe_request_report_id,
+        )
+    )
+
+
 def get_unsubscribe_request_reports_dao(service_id):
     return (
         UnsubscribeRequestReport.query.filter_by(service_id=service_id)
