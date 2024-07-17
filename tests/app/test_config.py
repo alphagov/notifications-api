62823d94e86bd0aa3961cfd2da045f2168888989
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-07-17 10:41:23 +0100

    update tests to reflect lower queue count

--- tests/app/test_config.py
+++ tests/app/test_config.py
@@ -7,7 +7,7 @@ from app.config import QueueNames
 def test_queue_names_all_queues_correct():
     # Need to ensure that all_queues() only returns queue names used in API
     queues = QueueNames.all_queues()
-    assert len(queues) == 19
+    assert len(queues) == 17
     assert {
         QueueNames.PERIODIC,
         QueueNames.DATABASE,
