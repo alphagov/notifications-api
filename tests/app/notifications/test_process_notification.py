3423f4d7f4163294aaa4a2270294786f0a5c5371
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2024-07-22 11:20:59 +0100

    passing test, correct exception raised

--- tests/app/notifications/test_process_notification.py
+++ tests/app/notifications/test_process_notification.py
@@ -6,6 +6,7 @@ import pytest
 from boto3.exceptions import Boto3Error
 from freezegun import freeze_time
 from notifications_utils.recipient_validation.email_address import validate_and_format_email_address
+from notifications_utils.recipient_validation.errors import InvalidPhoneError
 from notifications_utils.recipient_validation.phone_number import validate_and_format_phone_number
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -438,23 +439,24 @@ def test_persist_notification_without_send_to_landline_does_not_store(
     expected_prefix,
     expected_units,
 ):
-    persist_notification(
-        template_id=sample_job.template.id,
-        template_version=sample_job.template.version,
-        recipient=recipient,
-        service=sample_job.service,
-        personalisation=None,
-        notification_type="sms",
-        api_key_id=sample_api_key.id,
-        key_type=sample_api_key.key_type,
-        job_id=sample_job.id,
-        job_row_number=10,
-        client_reference="ref from client",
-    )
-    persisted_notification = Notification.query.all()[0]
-    assert persisted_notification.phone_prefix == expected_prefix
-    assert persisted_notification.normalised_to == expected_recipient_normalised
-    assert persisted_notification.rate_multiplier == expected_units
+    with pytest.raises(InvalidPhoneError):
+        persist_notification(
+            template_id=sample_job.template.id,
+            template_version=sample_job.template.version,
+            recipient=recipient,
+            service=sample_job.service,
+            personalisation=None,
+            notification_type="sms",
+            api_key_id=sample_api_key.id,
+            key_type=sample_api_key.key_type,
+            job_id=sample_job.id,
+            job_row_number=10,
+            client_reference="ref from client",
+        )
+    # persisted_notification = Notification.query.all()[0]
+    # assert persisted_notification.phone_prefix == expected_prefix
+    # assert persisted_notification.normalised_to == expected_recipient_normalised
+    # assert persisted_notification.rate_multiplier == expected_units
 
 
 def test_persist_notification_with_international_info_does_not_store_for_email(sample_job, sample_api_key, mocker):
