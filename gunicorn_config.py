4288ddfc64d4f9259ba4aa46950a1bce95429679
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2024-10-11 11:13:16 +0100

    EventletTimeoutMiddleware: wrap application.wsgi_app, get value from config/env var
    wrapping `application.wsgi_app` is more correct than wrapping
    `application` itself, as it preserves flasky behaviours of the
    outer `application` object
    
    also control the timeout time with a
    `HTTP_SERVE_TIMEOUT_SECONDS` config/env var that we can control
    from e.g. terraform, unifying this with the value we use for
    gunicorn's `timeout` (which has little effect on the eventlet
    `worker_class`, but controls a similar behaviour on non-async
    workers), with the intention we start to respect this across all
    our apps.
    
    also default to 30s is what we've decided to set our cloudfront
    timeouts to.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -11,3 +11,4 @@ worker_class = "eventlet"
 worker_connections = 256
 statsd_host = "{}:8125".format(os.getenv("STATSD_HOST"))
 keepalive = 90
+timeout = int(os.getenv("HTTP_SERVE_TIMEOUT_SECONDS", 30))  # though has little effect with eventlet worker_class
