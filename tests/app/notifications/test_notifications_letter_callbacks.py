0e616611edf2142592588c2efb94fca5c85ef3d2
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-10-16 17:06:36 +0100

    set dvla callback status to Title Case
    not UPPER CASE as we previously assumed based on their docs

--- tests/app/notifications/test_notifications_letter_callbacks.py
+++ tests/app/notifications/test_notifications_letter_callbacks.py
@@ -172,7 +172,7 @@ def test_process_letter_callback_gives_error_for_missing_or_invalid_token(client
         # invalid enum value for `jobStatus`
         (
             {"data": {"jobStatus": "INVALID_STATUS"}},
-            "data INVALID_STATUS is not one of [DESPATCHED, REJECTED]",
+            "data INVALID_STATUS is not one of [Despatched, Rejected]",
         ),
         # invalid `time` format
         (
@@ -282,7 +282,7 @@ def test_process_letter_callback_raises_error_if_token_and_notification_id_in_da
     )
 
 
-@pytest.mark.parametrize("status", ["DESPATCHED", "REJECTED"])
+@pytest.mark.parametrize("status", ["Despatched", "Rejected"])
 def test_process_letter_callback_calls_process_letter_callback_data_task(
     client,
     mocker,
@@ -360,7 +360,7 @@ def test_extract_properties_from_request(mock_dvla_callback_data):
                 {"key": "mailingProduct", "value": "MM UNSORTED"},
                 {"key": "Print Date", "value": "2024-08-01T09:15:14.456Z"},
             ],
-            "jobStatus": "REJECTED",
+            "jobStatus": "Rejected",
         }
     }
 
@@ -369,7 +369,7 @@ def test_extract_properties_from_request(mock_dvla_callback_data):
     letter_update = extract_properties_from_request(data)
 
     assert letter_update.page_count == "10"
-    assert letter_update.status == "REJECTED"
+    assert letter_update.status == "Rejected"
     assert letter_update.cost_threshold == LetterCostThreshold.unsorted
     assert letter_update.despatch_date == datetime.date(2024, 8, 1)
 
