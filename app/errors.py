7ea747ac254431f5d36bf71deb2c7c099b077d62
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2024-10-28 15:54:48 +0000

    other blueprint errors: add handler for EventletTimeout returning 504

--- app/errors.py
+++ app/errors.py
@@ -1,6 +1,7 @@
 from flask import current_app, json, jsonify
 from jsonschema import ValidationError as JsonSchemaValidationError
 from marshmallow import ValidationError
+from notifications_utils.eventlet import EventletTimeout
 from notifications_utils.recipient_validation.errors import InvalidRecipientError
 from sqlalchemy.exc import DataError
 from sqlalchemy.orm.exc import NoResultFound
@@ -96,6 +97,11 @@ def register_errors(blueprint):  # noqa: C901
     def no_result_found(e):
         current_app.logger.info(e)
         return jsonify(result="error", message="No result found"), 404
+
+    @blueprint.errorhandler(EventletTimeout)
+    def eventlet_timeout(error):
+        current_app.logger.exception(error)
+        return jsonify(result="error", message="Timeout serving request"), 504
 
     # this must be defined after all other error handlers since it catches the generic Exception object
     @blueprint.app_errorhandler(500)
