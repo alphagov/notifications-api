9fcbe9bbf3445fd4578e433f4c2a592a4d6a1889
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-10-31 14:41:24 +0000

    make migration tests more robust and errorproof
    * use pathlib.stem, not pathlib.name, so that we get the filename
      without the extension
    * write a new test that compares the alembic_version in the test DB
      with the filename to make sure that the "revision" field in the file
      matches the filename

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -6,7 +6,7 @@ def test_migration_files_start_with_numeric_id():
     """Make sure that all of the migration files start with a 4-character integer"""
     migrations = sorted(glob.glob("migrations/versions/*.py"))
 
-    migration_versions = [pathlib.Path(migration).name.split("_")[0] for migration in migrations]
+    migration_versions = [pathlib.Path(migration).stem.split("_")[0] for migration in migrations]
 
     assert all(
         migration_version.isdigit() and len(migration_version) == 4 for migration_version in migration_versions
@@ -15,8 +15,19 @@ def test_migration_files_start_with_numeric_id():
 
 def test_current_alembic_head():
     migrations = sorted(glob.glob("migrations/versions/*.py"))
-
-    head_migration_id = pathlib.Path(migrations[-1]).name.split(".")[0]
+    head_migration_filename = pathlib.Path(migrations[-1]).stem
 
     with open("migrations/.current-alembic-head") as current_alembic_head:
-        assert current_alembic_head.read() == f"{head_migration_id}\n"
+        assert current_alembic_head.read() == f"{head_migration_filename}\n"
+
+
+def test_revision_matches_filename(notify_db_session):
+    migrations = sorted(glob.glob("migrations/versions/*.py"))
+    head_migration_filename = pathlib.Path(migrations[-1]).stem
+    head_migration_id = head_migration_filename.split(".")[0]
+
+    results = notify_db_session.execute("select version_num from alembic_version").fetchall()
+    assert len(results) == 1
+    assert (
+        results[0].version_num == head_migration_id
+    ), 'alembic head filename {head_migration_filename} does not match "revision" {results[0].version}'
