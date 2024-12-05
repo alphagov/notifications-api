f9f032584dc2fc54ec37f09aac97ffd415670fbd
Pea Tyczynska <mervi.tyczynska@digital.cabinet-office.gov.uk>
2024-12-05 12:25:35 +0000

    Continue on slugs naming, and add some questions and discussion points

--- app/v2/errors/slugs.py
+++ app/v2/errors/slugs.py
@@ -1,21 +1,32 @@
 class ValidationErrorSlugs:
-    # phone numbers validation
+    # phone number validation
     TOO_MANY_DIGITS = "phone_number:too_many_digits"
     NOT_ENOUGH_DIGITS = "phone_number:not_enough_digits"
     NOT_A_UK_MOBILE_NUMBER = "phone_number:not_a_uk_mobile_number"
     NO_LETTERS_OR_SYMBOLS = "phone_number:remove_letters_and_symbols"
     INVALID_COUNTRY_PREFIX = "phone_number:invalid_country_prefix"
+    PHONE_NUMBER_REQUIRED = "phone_number:required_property"
 
-    # required property 
-    REQUIRED_PHONE_NUMBER = "required_property:phone_number"
-    REQUIRED_EMAIL_ADDRESS = "required_property:email_address"
-    REQUIRED_TEMPLATE_ID = "required_property:template_id"
-    REQUIRED_PERSONALISATION = "required_property:personalisation"
-    REQUIRED_NOTIFICATION_ID = "required_property:notification_id"
-    REQUIRED_REFERENCE = "required_property:reference" # for precompiled letters only
-    REQUIRED_CONTENT = "required_property:content"
+    # email address validation
+    EMAIL_ADDRESS_REQUIRED = "email_address:required_property"
 
-    # scheduled notification
-    SCHEDULED_NOTIFICATION_IN_PAST = "scheduled_notification:cannot_be_in_past"
-    SCHEDULED_NOTIFICATION_EXCEEDS_TWENTY_FOUR_HOURS = "scheduled_notification:cannot_be_greater_than_twenty_four_hours"
-    SCHEDULED_FOR_INVALID_DATETIME = "scheduled_notification:not_a_valid_datetime"
+    # template id validation
+    TEMPLATE_ID_REQUIRED = "template_id:required_property"
+    TEMPLATE_ID_INVALID = "template_id:invalid"
+
+    PERSONALISATION_REQUIRED = "personalisation:required_property"
+
+    NOTIFICATION_ID_REQUIRED = "notification_id:required_property"
+
+    REFERENCE_REQUIRED = "reference:required_property"  # for precompiled letters only
+
+    CONTENT_REQUIRED = "content:required_property"
+
+    """
+    QUESTIONS / DISCUSSION POINTS
+    1. For this test: test_get_complaint_with_invalid_data_returns_400_status_code, the error message is
+    'start_date month must be in 1..12'. It comes from e.path + e.cause. How exact we want to get wit our slugs
+    - do we want a separate slug for day, month, and year in the date, like `start_date:month_must_be_in_1..1`,
+    or do we want to be a bit more vague, for example `start_date:format`? How much resolution do we want?
+
+    """
