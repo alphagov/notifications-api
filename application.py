d9b616aa4655aa0cbe267d404ee48250c6266114
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2024-12-23 18:29:19 +0000

    add handling of NOTIFY_EVENTLET_STATS
    if we're using eventlet and NOTIFY_EVENTLET_STATS is set to 1,
    we install the account_greenlet_times greenlet trace hook when
    initializing the app
    
    this should allow notifications_utils.eventlet's greenlet timing
    and statistics functions to work and will annotate some of these
    values onto app.request log messages.
    
    this is a flag that should only be enabled when we're actively
    tracking down an issue as there is probably a (minor) performance
    penalty for calling this hook on every context switch.

--- application.py
+++ application.py
@@ -8,14 +8,19 @@ init_performance_monitoring()
 from app import create_app  # noqa
 from app.notify_api_flask_app import NotifyApiFlaskApp  # noqa
 
-from notifications_utils.eventlet import EventletTimeoutMiddleware, using_eventlet  # noqa
+import notifications_utils.eventlet as utils_eventlet  # noqa
 
 application = NotifyApiFlaskApp("app")
 
 create_app(application)
 
-if using_eventlet:
-    application.wsgi_app = EventletTimeoutMiddleware(
+if utils_eventlet.using_eventlet:
+    application.wsgi_app = utils_eventlet.EventletTimeoutMiddleware(
         application.wsgi_app,
         timeout_seconds=int(os.getenv("HTTP_SERVE_TIMEOUT_SECONDS", 30)),
     )
+
+    if application.config["NOTIFY_EVENTLET_STATS"]:
+        import greenlet
+
+        greenlet.settrace(utils_eventlet.account_greenlet_times)
