3fa8c58a84ca9fa30b2da668e0d3dc6067fe1ac9
Chukwugozie Mbeledogu <chukwugozie.mbeledogu@digital.cabinet-office.gov.uk>
2025-01-16 12:40:34 +0000

    insert inbound_sms callback type to service_callback_type table  This lays the foundation for future work to merge the content of service_inbound_api into the service_callback_api

--- migrations/versions/0486_insert_returned_letter.py
+++ migrations/versions/0486_insert_returned_letter.py
@@ -11,9 +11,13 @@ down_revision = "0485_add_job_status_fin_allrws"
 
 def upgrade():
     insert_returned_letter_callback_type = "INSERT INTO service_callback_type VALUES ('returned_letters')"
+    insert_inbound_sms_callback_type = "INSERT INTO service_callback_type VALUES ('inbound_sms')"
     op.execute(insert_returned_letter_callback_type)
+    op.execute(insert_inbound_sms_callback_type)
 
 
 def downgrade():
     delete_returned_letter_callback_type = "DELETE FROM service_callback_type WHERE name = 'returned_letters'"
+    delete_inbound_sms_callback_type = "DELETE FROM service_callback_type WHERE name = 'inbound_sms'"
     op.execute(delete_returned_letter_callback_type)
+    op.execute(delete_inbound_sms_callback_type)
