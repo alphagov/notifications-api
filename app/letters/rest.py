dc9942df3065c87ec0dae43320c95b2d3df18979
Chukwugozie Mbeledogu <chukwugozie.mbeledogu@digital.cabinet-office.gov.uk>
2025-01-24 15:18:17 +0000

    Add fetch_returned_letter_data  this method fetches the returned letter data needed for both the returned letter report  and the returned letter callback.

--- app/letters/rest.py
+++ app/letters/rest.py
@@ -3,8 +3,10 @@ from flask import Blueprint, jsonify, request
 from app.celery.tasks import process_returned_letters_list
 from app.config import QueueNames
 from app.dao.letter_rate_dao import dao_get_current_letter_rates
+from app.dao.returned_letters_dao import fetch_returned_letters
 from app.letters.letter_schemas import letter_references
 from app.schema_validation import validate
+from app.utils import DATE_FORMAT, DATETIME_FORMAT_NO_TIMEZONE
 from app.v2.errors import register_errors
 
 letter_job = Blueprint("letter-job", __name__)
@@ -33,3 +35,28 @@ def create_process_returned_letters_job():
 @letter_rates_blueprint.route("/", methods=["GET"])
 def get_letter_rates():
     return jsonify([rate.serialize() for rate in dao_get_current_letter_rates()])
+
+
+def fetch_returned_letter_data(service_id, report_date):
+    results = fetch_returned_letters(service_id=service_id, report_date=report_date)
+    json_results = [
+        {
+            "notification_id": x.notification_id,
+            # client reference can only be added on API letters
+            "client_reference": x.client_reference if x.api_key_id else None,
+            "reported_at": x.reported_at.strftime(DATE_FORMAT),
+            "created_at": x.created_at.strftime(DATETIME_FORMAT_NO_TIMEZONE),
+            # it doesn't make sense to show hidden/precompiled templates
+            "template_name": x.template_name if not x.hidden else None,
+            "template_id": x.template_id if not x.hidden else None,
+            "template_version": x.template_version if not x.hidden else None,
+            "user_name": x.user_name or "API",
+            "email_address": x.email_address or "API",
+            "original_file_name": x.original_file_name,
+            "job_row_number": x.job_row_number,
+            # the file name for a letter uploaded via the UI
+            "uploaded_letter_file_name": x.client_reference if x.hidden and not x.api_key_id else None,
+        }
+        for x in results
+    ]
+    return json_results
