902c6c251a74d9736d7fd59acb5ffe69c7fec1da
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2025-01-27 14:12:40 +0000

    set server default=false to save having to do multiple migrations

--- migrations/versions/0487_add_annual_billing_columns.py
+++ migrations/versions/0487_add_annual_billing_columns.py
@@ -11,8 +11,8 @@ down_revision = "0486_letter_rates_feb_2025"
 
 
 def upgrade():
-    op.add_column("annual_billing", sa.Column("high_volume_service_last_year", sa.Boolean(), nullable=True))
-    op.add_column("annual_billing", sa.Column("has_custom_allowance", sa.Boolean(), nullable=True))
+    op.add_column("annual_billing", sa.Column("high_volume_service_last_year", sa.Boolean(), server_default=False))
+    op.add_column("annual_billing", sa.Column("has_custom_allowance", sa.Boolean(), server_default=False))
 
 
 def downgrade():
