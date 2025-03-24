23bf3e3124f9eb0c8cfa66111c3709279de6c909
spatel033 <177532680+spatel033@users.noreply.github.com>
2025-03-24 14:10:01 +0000

    Add get notification by batch
    Processing the batch of the notifications for request-reports.

--- app/report_requests/process_notifications_report.py
+++ app/report_requests/process_notifications_report.py
@@ -1,3 +1,18 @@
+import csv
+from io import StringIO
+
+from flask import current_app
+from notifications_utils.s3 import (
+    s3_multipart_upload_abort,
+    s3_multipart_upload_complete,
+    s3_multipart_upload_create,
+    s3_multipart_upload_part,
+)
+
+from app.constants import NOTIFICATION_REPORT_REQUEST_MAPPING
+from app.dao.notifications_dao import get_notifications_for_service
+
+
 def convert_notifications_to_csv(serialized_notifications):
     values = []
     for notification in serialized_notifications:
@@ -17,3 +32,21 @@ def convert_notifications_to_csv(serialized_notifications):
             )
         )
     return values
+
+
+def get_notifications_by_batch(service_id, notification_status, template_type, page, page_size, limit_days):
+    statuses = NOTIFICATION_REPORT_REQUEST_MAPPING[notification_status]
+
+    notifications = get_notifications_for_service(
+        service_id=service_id,
+        filter_dict={
+            "template_type": template_type,
+            "status": statuses,
+        },
+        page=page,
+        page_size=page_size,
+        limit_days=limit_days,
+    )
+
+    serialized_notifications = [notification.serialize_for_csv() for notification in notifications]
+    return serialized_notifications
--- tests/app/report_requests/test_process_notifications_report.py
+++ tests/app/report_requests/test_process_notifications_report.py
@@ -1,18 +1,29 @@
+import pytest
 from freezegun import freeze_time
 
 from app.constants import (
     KEY_TYPE_NORMAL,
+    NOTIFICATION_CREATED,
+    NOTIFICATION_DELIVERED,
+    NOTIFICATION_REQUEST_REPORT_ALL,
+    NOTIFICATION_REQUEST_REPORT_DELIVERED,
+    NOTIFICATION_REQUEST_REPORT_SENDING,
+    NOTIFICATION_SENDING,
+    NOTIFICATION_SENT,
+    REPORT_REQUEST_NOTIFICATIONS,
 )
 from app.dao.notifications_dao import (
     get_notifications_for_service,
 )
 from app.report_requests.process_notifications_report import (
     convert_notifications_to_csv,
+    get_notifications_by_batch,
 )
 from tests.app.db import (
     create_api_key,
     create_notification,
     create_service,
+    create_service_data_retention,
 )
 
 
@@ -103,3 +114,44 @@ def test_convert_notifications_to_csv_values(sample_sms_template):
     ]
 
     assert expected_csv == csv_data
+
+
+@pytest.mark.parametrize(
+    "page_size, page, expected_notifications, notification_report_request_status",
+    [
+        (5, 1, 5, NOTIFICATION_REQUEST_REPORT_ALL),
+        (2, 2, 2, NOTIFICATION_REQUEST_REPORT_SENDING),
+        (1, 1, 1, NOTIFICATION_REQUEST_REPORT_SENDING),
+        (2, 1, 2, NOTIFICATION_REQUEST_REPORT_DELIVERED),
+    ],
+)
+def test_get_notifications_by_batch(
+    page_size,
+    page,
+    notification_report_request_status,
+    expected_notifications,
+    sample_email_template,
+    sample_sms_template,
+):
+    service = create_service(check_if_service_exists=True)
+    create_service_data_retention(service=service)
+    api_key = create_api_key(service=service, key_type=KEY_TYPE_NORMAL, id="8e33368c-3965-4ae1-ab55-4f9d3275f84d")
+
+    create_notification(template=sample_email_template, status=NOTIFICATION_SENDING, api_key=api_key)
+    create_notification(template=sample_sms_template, status=NOTIFICATION_SENDING, api_key=api_key)
+    create_notification(template=sample_email_template, status=NOTIFICATION_DELIVERED, api_key=api_key)
+    create_notification(template=sample_email_template, status=NOTIFICATION_DELIVERED, api_key=api_key)
+    create_notification(template=sample_email_template, status=NOTIFICATION_SENDING, api_key=api_key)
+    create_notification(template=sample_email_template, status=NOTIFICATION_SENDING, api_key=api_key)
+    create_notification(template=sample_email_template, status=NOTIFICATION_CREATED, api_key=api_key)
+    create_notification(template=sample_email_template, status=NOTIFICATION_SENT, api_key=api_key)
+
+    notifications = get_notifications_by_batch(
+        service_id=service.id,
+        notification_status=notification_report_request_status,
+        template_type="email",
+        page=page,
+        page_size=page_size,
+        limit_days=2,
+    )
+    assert len(notifications) == expected_notifications
