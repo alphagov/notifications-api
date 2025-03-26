7d507b8f50ce665452f663339ec1a4dc88fe710e
spatel033 <177532680+spatel033@users.noreply.github.com>
2025-03-26 09:29:58 +0000

    Add process for notification request report
    Process the request report for the notification using the request ID. Retrieve notifications in batches and convert them into CSV format.
    Once a batch is converted, upload it to S3 using multipart upload. This approach ensures efficient handling of large CSV files.

--- app/report_requests/process_notifications_report.py
+++ app/report_requests/process_notifications_report.py
@@ -1,8 +1,10 @@
 import csv
+import sys
 from io import StringIO
 
 from flask import current_app
 from notifications_utils.s3 import (
+    S3_MULTIPART_UPLOAD_MIN_PART_SIZE,
     s3_multipart_upload_abort,
     s3_multipart_upload_complete,
     s3_multipart_upload_create,
@@ -11,6 +13,111 @@ from notifications_utils.s3 import (
 
 from app.constants import NOTIFICATION_REPORT_REQUEST_MAPPING
 from app.dao.notifications_dao import get_notifications_for_service
+from app.dao.report_requests_dao import dao_get_report_request_by_id
+from app.dao.service_data_retention_dao import fetch_service_data_retention_by_notification_type
+
+
+def process_report_request(service_id, report_request_id):
+    report_request = dao_get_report_request_by_id(service_id, report_request_id)
+    notification_type = report_request.parameter["notification_type"]
+    notification_status = report_request.parameter["notification_status"]
+
+    page_size = current_app.config.get("REPORT_REQUEST_NOTIFICATIONS_CSV_BATCH_SIZE")
+    s3_bucket = current_app.config["S3_BUCKET_REPORT_REQUESTS_DOWNLOAD"]
+    filename = f"notifications_report/{report_request_id}.csv"
+    part_number = 1
+
+    init_upload_response = s3_multipart_upload_create(s3_bucket, filename)
+    upload_id = init_upload_response["UploadId"]
+    parts = []
+
+    csv_buffer = StringIO()
+    csv_writer = csv.writer(csv_buffer)
+    accumulated_size = 0
+
+    headers = [
+        "Recipient",
+        "Reference",
+        "Template",
+        "Type",
+        "Sent by",
+        "Sent by email",
+        "Job",
+        "Status",
+        "Time",
+        "API key name",
+    ]
+    csv_writer.writerow(headers)
+
+    try:
+        service_retention = fetch_service_data_retention_by_notification_type(service_id, notification_type)
+        limit_days = service_retention.days_of_retention if service_retention else 7
+        page = 1
+
+        while True:
+            serialized_notifications = get_notifications_by_batch(
+                service_id=service_id,
+                template_type=notification_type,
+                notification_status=notification_status,
+                page=page,
+                page_size=page_size,
+                limit_days=limit_days,
+            )
+
+            csv_data = convert_notifications_to_csv(serialized_notifications)
+            csv_writer.writerows(csv_data)
+
+            csv_buffer.seek(0)
+            data_bytes = csv_buffer.getvalue().encode("utf-8")
+
+            accumulated_size += sys.getsizeof(data_bytes)
+
+            # upload when the minimum upload part size exceeds
+            if accumulated_size >= S3_MULTIPART_UPLOAD_MIN_PART_SIZE:
+                upload_response = s3_multipart_upload_part(
+                    part_number=part_number,
+                    bucket_name=s3_bucket,
+                    filename=filename,
+                    upload_id=upload_id,
+                    data_bytes=data_bytes,
+                )
+                parts.append({"PartNumber": part_number, "ETag": upload_response["ETag"]})
+                part_number += 1
+
+                # clear the buffer for the next batch
+                csv_buffer.truncate(0)
+                csv_buffer.seek(0)
+                accumulated_size = 0
+
+            page += 1
+
+            if len(serialized_notifications) < page_size:
+                break
+
+        # upload any remaining data
+        if accumulated_size:
+            csv_buffer.seek(0)
+            data_bytes = csv_buffer.getvalue().encode("utf-8")
+            upload_response = s3_multipart_upload_part(
+                part_number=part_number,
+                bucket_name=s3_bucket,
+                filename=filename,
+                upload_id=upload_id,
+                data_bytes=data_bytes,
+            )
+            parts.append({"PartNumber": part_number, "ETag": upload_response["ETag"]})
+
+        s3_multipart_upload_complete(
+            bucket_name=s3_bucket,
+            filename=filename,
+            upload_id=upload_id,
+            parts=parts,
+        )
+
+    except Exception as e:
+        current_app.logger.exception("Error occurred for process notification request report: %s", e)
+        s3_multipart_upload_abort(bucket_name=s3_bucket, filename=filename, upload_id=upload_id)
+        raise e
 
 
 def convert_notifications_to_csv(serialized_notifications):
@@ -46,6 +153,7 @@ def get_notifications_by_batch(service_id, notification_status, template_type, p
         page=page,
         page_size=page_size,
         limit_days=limit_days,
+        error_out=False,
     )
 
     serialized_notifications = [notification.serialize_for_csv() for notification in notifications]
