64aeb251abc62b1ec6ba4a63328002bf00daa2dc
Mervi Tyczynska <mervi.tyczynska@digital.cabinet-office.gov.uk>
2025-04-11 13:22:21 +0100

    Set and increment international sms daily limit cache.

--- app/notifications/process_notifications.py
+++ app/notifications/process_notifications.py
@@ -183,11 +183,17 @@ def persist_notification(
     # if simulated create a Notification model to return but do not persist the Notification to the dB
     if not simulated:
         dao_create_notification(notification)
-        increment_daily_limit_cache(service.id, notification_type, key_type)
+        increment_daily_limit_cache(
+            service.id,
+            notification_type,
+            key_type,
+            international_sms=notification.international,
+        )
+
     return notification
 
 
-def increment_daily_limit_cache(service_id, notification_type, key_type):
+def increment_daily_limit_cache(service_id, notification_type, key_type, international_sms=False):
     if key_type == KEY_TYPE_TEST or not current_app.config["REDIS_ENABLED"]:
         return
 
@@ -201,6 +207,21 @@ def increment_daily_limit_cache(service_id, notification_type, key_type):
             redis_store.set(cache_key, 1, ex=86400)
         else:
             redis_store.incr(cache_key)
+
+    if international_sms:
+        _increment_international_sms_daily_limit_cache(service_id)
+
+
+def _increment_international_sms_daily_limit_cache(service_id):
+    cache_key = redis.daily_limit_cache_key(service_id, notification_type="international-sms")
+    if redis_store.get(cache_key) is None:
+        # if cache does not exist set the cache to 1 with an expiry of 24 hours,
+        # The cache should be set by the time we create the notification
+        # but in case it is this will make sure the expiry is set to 24 hours,
+        # where if we let the incr method create the cache it will be set a ttl.
+        redis_store.set(cache_key, 1, ex=86400)
+    else:
+        redis_store.incr(cache_key)
 
 
 def send_notification_to_queue_detached(key_type, notification_type, notification_id, queue=None):
