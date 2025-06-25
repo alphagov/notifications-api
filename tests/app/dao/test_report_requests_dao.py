8a9021f12af46b0f689e419d534387456e6c69e8
spatel033 <177532680+spatel033@users.noreply.github.com>
2025-06-25 16:30:30 +0100

    Reports marked as deleted should return a not found (404) error
    Users should not be able to access status reports that have been deleted by the admin. The report’s CSV file is deleted from S3 after one day, and a scheduled job marks the report as ‘deleted’ so the data can still be used for investigation. The admin handles 404 errors, so until the row is removed from the database, users should see a report unavailable message in the admin panel.

--- tests/app/dao/test_report_requests_dao.py
+++ tests/app/dao/test_report_requests_dao.py
@@ -2,8 +2,10 @@ from datetime import datetime, timedelta
 
 import pytest
 from flask import current_app
+from sqlalchemy.orm.exc import NoResultFound
 
 from app.constants import (
+    REPORT_REQUEST_DELETED,
     REPORT_REQUEST_FAILED,
     REPORT_REQUEST_IN_PROGRESS,
     REPORT_REQUEST_NOTIFICATIONS,
@@ -11,6 +13,7 @@ from app.constants import (
 )
 from app.dao.report_requests_dao import (
     dao_create_report_request,
+    dao_get_active_report_request_by_id,
     dao_get_oldest_ongoing_report_request,
     dao_get_report_request_by_id,
     dao_update_report_request,
@@ -344,3 +347,44 @@ def test_dao_update_report_request(sample_service, sample_user):
 
     assert report_request.status == REPORT_REQUEST_IN_PROGRESS
     assert report_request.updated_at
+
+
+def test_dao_get_active_report_request_by_id_when_deleted_report(sample_service, sample_user):
+    sample_parameter = {"notification_status": "sending"}
+
+    report_request = ReportRequest(
+        user_id=sample_user.id,
+        service_id=sample_service.id,
+        report_type=REPORT_REQUEST_NOTIFICATIONS,
+        status=REPORT_REQUEST_DELETED,
+        parameter=sample_parameter,
+    )
+
+    dao_create_report_request(report_request)
+
+    with pytest.raises(NoResultFound) as e:
+        dao_get_active_report_request_by_id(sample_service.id, report_request.id)
+
+    assert "No row was found when one was required" in str(e.value)
+
+
+def test_dao_get_active_report_request_by_id_when_active_report(sample_service, sample_user):
+    sample_parameter = {"notification_status": "sending"}
+
+    report_request = ReportRequest(
+        user_id=sample_user.id,
+        service_id=sample_service.id,
+        report_type=REPORT_REQUEST_NOTIFICATIONS,
+        status=REPORT_REQUEST_IN_PROGRESS,
+        parameter=sample_parameter,
+    )
+
+    dao_create_report_request(report_request)
+    report = dao_get_report_request_by_id(sample_service.id, report_request.id)
+
+    assert report.id == report_request.id
+    assert report.service_id == sample_service.id
+    assert report.user_id == sample_user.id
+    assert report.report_type == REPORT_REQUEST_NOTIFICATIONS
+    assert report.status == REPORT_REQUEST_IN_PROGRESS
+    assert report.parameter == sample_parameter
