16ec9b760410b21148bb06ae99d8190c4f3179e8
spatel033 <177532680+spatel033@users.noreply.github.com>
2025-07-28 10:04:40 +0100

    Fix missing service field in TemplateSchema for Sqlalchemy 2.0

--- app/schemas.py
+++ app/schemas.py
@@ -426,6 +426,7 @@ class TemplateSchema(BaseTemplateSchema, UUIDsAsStringsMixin):
     is_precompiled_letter = fields.Method("get_is_precompiled_letter")
     created_at = FlexibleDateTime()
     updated_at = FlexibleDateTime()
+    service = fields.Nested(ServiceSchema)
 
     def redact(self, template):
         return template.redact_personalisation
