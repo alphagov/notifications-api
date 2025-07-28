45b69f624daea1af8a24cf14cdeafe3457052bd4
spatel033 <177532680+spatel033@users.noreply.github.com>
2025-07-28 10:05:43 +0100

    Update QueryRecorder to use db.get_debug_queries for Sqlalchemy 2.0
    The get_debug_queries function was removed from the top-level flask_sqlalchemy module in a recent version of the library and is now a method on the SQLAlchemy 2 instance (db). This change updates the QueryRecorder test utility to accept the db instance and call db.get_debug_queries() instead of the old, removed function.

--- tests/utils.py
+++ tests/utils.py
@@ -7,8 +7,8 @@ class QueryRecorder:
         self._count_on_enter = None
 
     def __enter__(self):
-        self._count_on_enter = len(flask_sqlalchemy.get_debug_queries())
+        self._count_on_enter = len(flask_sqlalchemy.record_queries.get_recorded_queries())
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.queries = flask_sqlalchemy.get_debug_queries()[self._count_on_enter :]
+        self.queries = flask_sqlalchemy.record_queries.get_recorded_queries()[self._count_on_enter :]
