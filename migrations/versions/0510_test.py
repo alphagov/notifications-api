aa83ecdb827324dd6082caba0a0679c9175305d4
spatel033 <177532680+spatel033@users.noreply.github.com>
2025-07-29 17:20:14 +0100

    test migration

---
+++ migrations/versions/0510_test.py
@@ -0,0 +1,17 @@
+"""
+Create Date: 2025-07-07 15:46:53.673184
+"""
+
+from alembic import op
+from sqlalchemy import text
+
+revision = '0510_test'
+down_revision = '0509_delete_broadcast_data'
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
