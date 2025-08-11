b48f2f24eb6051b9699766e3ec42f17d34aad768
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2025-08-11 18:55:09 +0100

    Use token bucket algorithm instead of sliding window in check_service_over_api_rate_limit validator
    The impact of bucket size varies depending on the rate limit (bucket
    replenish rate) of the service.
    
    We are choosing a rate such that users with a per minute rate of 3000
    requests per minute should aim to send smoothly (50 requests per second)
    but they can send up to 1,000 times per second but will get throttled to
    keep the average limit at 50 per second.
    
    This is only for services who have the permission, so none will be
    affected by this when we initially deploy it.
    
    Values are chosen to avoid any impact on the vast majority of services,
    while giving errors to the handful who show big spikes.
    
    See https://docs.google.com/document/d/1Px21_Seg_kfaSktscGY4XWC80tdwbwxcX8OXIB65TUI/edit

--- app/notifications/validators.py
+++ app/notifications/validators.py
@@ -19,8 +19,11 @@ from app.constants import (
     KEY_TYPE_TEAM,
     KEY_TYPE_TEST,
     LETTER_TYPE,
+    SECONDS_IN_1_MINUTE,
     SMS_TO_UK_LANDLINES,
     SMS_TYPE,
+    TOKEN_BUCKET_MAX,
+    TOKEN_BUCKET_MIN,
 )
 from app.dao.service_email_reply_to_dao import dao_get_reply_to_by_id
 from app.dao.service_letter_contact_dao import dao_get_letter_contact_by_id
@@ -45,14 +48,32 @@ REDIS_EXCEEDED_RATE_LIMIT_DURATION_SECONDS = Histogram(
 
 
 def check_service_over_api_rate_limit(service, key_type):
-    if current_app.config["API_RATE_LIMIT_ENABLED"] and current_app.config["REDIS_ENABLED"]:
-        cache_key = rate_limit_cache_key(service.id, key_type)
-        rate_limit = service.rate_limit
-        interval = 60
-        with REDIS_EXCEEDED_RATE_LIMIT_DURATION_SECONDS.time():
-            if redis_store.exceeded_rate_limit(cache_key, rate_limit, interval):
-                current_app.logger.info("service %s has been rate limited for throughput", service.id)
-                raise RateLimitError(rate_limit, interval, key_type)
+    if not current_app.config["API_RATE_LIMIT_ENABLED"]:
+        return
+    if not current_app.config["REDIS_ENABLED"]:
+        return
+
+    with REDIS_EXCEEDED_RATE_LIMIT_DURATION_SECONDS.time():
+        if rate_limit_exceeded(service, key_type):
+            current_app.logger.info("service %s has been rate limited for throughput", service.id)
+            raise RateLimitError(service.rate_limit, SECONDS_IN_1_MINUTE, key_type)
+
+
+def rate_limit_exceeded(service, key_type):
+    cache_key = rate_limit_cache_key(service.id, key_type)
+
+    if service.has_permission("token_bucket"):
+        return (
+            redis_store.get_remaining_bucket_tokens(
+                key=cache_key,
+                replenish_per_sec=int(service.rate_limit / SECONDS_IN_1_MINUTE),
+                bucket_max=TOKEN_BUCKET_MAX,
+                bucket_min=TOKEN_BUCKET_MIN,
+            )
+            < 1
+        )
+
+    return redis_store.exceeded_rate_limit(cache_key, service.rate_limit, SECONDS_IN_1_MINUTE)
 
 
 def check_service_over_daily_message_limit(service, key_type, notification_type, num_notifications=1):
