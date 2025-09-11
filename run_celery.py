62ce4af924915870cb97f82b00ed9fd4844a183c
Chukwugozie Mbeledogu <chukwugozie.mbeledogu@digital.cabinet-office.gov.uk>
2025-09-11 14:01:28 +0100

    Update run_celery.py to remove import of pycurl  celery 5.5.3 replaces pycurl with urllib3

--- run_celery.py
+++ run_celery.py
@@ -11,16 +11,6 @@ init_performance_monitoring()
 # prometheus will simply store the metrics in memory
 import prometheus_client  # noqa
 
-# We have lots of issues related to using pycurl locally on (M1?) macs.
-# We have specific installation instructions for pycurl here:
-#   https://github.com/alphagov/notifications-manuals/wiki/Getting-started#pycurl
-# However this sometimes still doesn't seem to be enough for pycurl when running the celery workers.
-# Importing pycurl here - notably before the import at kombu.asynchronous.http.curl:15 - seems to mitigate the error
-# I've seen a lot lately:
-#   ImportError: pycurl: libcurl link-time version (7.76.1) is older than compile-time version (7.85.0)
-# See https://github.com/alphagov/notifications-api/pull/3687 for a little more of the investigation/notes
-import pycurl  # noqa
-
 # notify_celery is referenced from manifest_delivery_base.yml, and cannot be removed
 from app import create_app, notify_celery  # noqa
 from app.notify_api_flask_app import NotifyApiFlaskApp  # noqa
