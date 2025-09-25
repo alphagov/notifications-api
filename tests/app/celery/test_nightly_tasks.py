77674d78bc00087e1bebaa01de09e221c46be1ff
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2025-09-25 14:00:28 +0100

    TMP

--- tests/app/celery/test_nightly_tasks.py
+++ tests/app/celery/test_nightly_tasks.py
@@ -1,3 +1,4 @@
+import logging
 import uuid
 from datetime import date, datetime, timedelta
 from unittest.mock import ANY, call
@@ -769,7 +770,7 @@ def test_delete_unneeded_notification_history_for_specific_hour2(mocker):
 
 
 @freeze_time("2021-02-04 10:11")
-def test_deep_archive_notification_history_up_to_limit(notify_db_session, notify_api, sample_template, mocker):
+def test_deep_archive_notification_history_up_to_limit(caplog, notify_db_session, notify_api, sample_template, mocker):
     from tests.conftest import set_config
     with set_config(notify_api, "NOTIFICATION_DEEP_HISTORY_DELETE_ARCHIVED", False):
         create_notification_history(sample_template, created_at=datetime(2020, 2, 3, 4, 0, 0))
@@ -781,7 +782,7 @@ def test_deep_archive_notification_history_up_to_limit(notify_db_session, notify
         create_notification_history(sample_template, created_at=datetime(2020, 2, 5, 9, 23, 23))
         create_notification_history(sample_template, created_at=datetime(2020, 2, 5, 10, 0, 0))
 
-        mocker.patch(
+        mock_inner = mocker.patch(
             "app.celery.nightly_tasks._deep_archive_notification_history_hour_starting",
             side_effect=(
                 datetime(2020, 2, 3, 4, 10, 2),
@@ -791,3 +792,16 @@ def test_deep_archive_notification_history_up_to_limit(notify_db_session, notify
         )
 
         deep_archive_notification_history_up_to_limit()
+
+        assert mock_inner.mock_calls == [
+            call("2020-02-03T04:00:00"),
+            call("2020-02-03T05:00:00"),
+            call("2020-02-05T09:00:00"),
+        ]
+
+        assert caplog.record_tuples == [
+            ("test", logging.INFO, "Archiving created_at hour beginning 2020-02-03T04:00:00"),
+            ("test", logging.INFO, "Archiving created_at hour beginning 2020-02-03T05:00:00"),
+            ("test", logging.INFO, "Archiving created_at hour beginning 2020-02-05T09:00:00"),
+            ("test", logging.INFO, "No more archivable notification_history rows"),
+        ]
