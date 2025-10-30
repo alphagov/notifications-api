b8572fc21df5604b1d8830ee961ea98bf16def58
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2025-10-30 15:09:12 +0000

    alembic migration

---
+++ migrations/versions/0538_update_constraints_templates_email_files.py
@@ -0,0 +1,20 @@
+"""
+Create Date: 2025-10-30 15:00:54.790811
+"""
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = '01ce5c92472b'
+down_revision = '0537_templates_email_files'
+
+
+def upgrade():
+    op.execute("ALTER TABLE template_email_files ALTER COLUMN archived_by_id DROP NOT NULL")
+    op.execute("ALTER TABLE template_email_files_history ALTER COLUMN archived_by_id DROP NOT NULL")
+
+
+def downgrade():
+    op.execute("ALTER TABLE template_email_files ALTER COLUMN archived_by_id SET NOT NULL")
+    op.execute("ALTER TABLE template_email_files_history ALTER COLUMN archived_by_id SET NOT NULL")
