a4b8531b133c63125e0b65f6e51f2848449176cf
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2025-11-14 16:01:26 +0000

    updated test_dao_get_template_email_files_by_template_id method to use sample_template_email_file fixture. less boilerplate

--- tests/app/dao/test_template_email_files_dao.py
+++ tests/app/dao/test_template_email_files_dao.py
@@ -57,27 +57,15 @@ def test_dao_get_template_email_file_by_id(sample_email_template, sample_service
     assert template_email_file_fetched.created_by_id == sample_service.users[0].id
 
 
-def test_dao_get_template_email_files_by_template_id(sample_email_template, sample_service):
-    data = {
-        "id": "d963f496-b075-4e13-90ae-1f009feddbc6",
-        "filename": "example.pdf",
-        "link_text": "click this link!",
-        "retention_period": 90,
-        "validate_users_email": True,
-        "template_id": str(sample_email_template.id),
-        "template_version": int(sample_email_template.version),
-        "created_by_id": str(sample_service.users[0].id),
-    }
-    template_email_file = TemplateEmailFile(**data)
-    dao_create_template_email_files(template_email_file)
-    template_email_file_fetched = dao_get_template_email_files_by_template_id(str(sample_email_template.id))[0]
-    assert str(template_email_file_fetched.id) == "d963f496-b075-4e13-90ae-1f009feddbc6"
-    assert template_email_file_fetched.filename == "example.pdf"
-    assert template_email_file_fetched.link_text == "click this link!"
-    assert template_email_file_fetched.retention_period == 90
-    assert template_email_file_fetched.validate_users_email
-    assert template_email_file_fetched.template_version == int(sample_email_template.version)
-    assert template_email_file_fetched.created_by_id == sample_service.users[0].id
+def test_dao_get_template_email_files_by_template_id(sample_template_email_file):
+    template_email_file_fetched = dao_get_template_email_files_by_template_id(str(sample_template_email_file.template_id))[0]
+    assert template_email_file_fetched.id == sample_template_email_file.id
+    assert template_email_file_fetched.filename == sample_template_email_file.filename
+    assert template_email_file_fetched.link_text == sample_template_email_file.link_text
+    assert template_email_file_fetched.retention_period == sample_template_email_file.retention_period
+    assert template_email_file_fetched.validate_users_email == sample_template_email_file.validate_users_email
+    assert template_email_file_fetched.template_version == sample_template_email_file.template_version
+    assert template_email_file_fetched.created_by_id == sample_template_email_file.created_by_id
 
 
 def test_dao_get_template_email_files_by_template_id_historical(sample_email_template, sample_service):
