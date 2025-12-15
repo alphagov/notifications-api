1a442c36b3e858b27891aabd469d8eca6bbbf5ef
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2025-12-15 15:20:49 +0000

    notifications_not_yet_sent, letters_missing_from_sending_bucket: query in time chunks
    to reduce the maxiumum potential query time when run on
    the read replica, so we can avoid replication conflicts
    
    also rename/type args for more clarity

--- tests/app/dao/notification_dao/test_notification_dao.py
+++ tests/app/dao/notification_dao/test_notification_dao.py
@@ -1596,14 +1596,12 @@ def test_dao_get_notification_or_history_by_reference_with_no_matches_raises_err
 @pytest.mark.parametrize("session", (db.session, db.session_bulk), ids=("default", "bulk"))
 @pytest.mark.parametrize("notification_type", ["letter", "email", "sms"])
 def test_notifications_not_yet_sent(sample_service, notification_type, session):
-    older_than = 4  # number of seconds the notification can not be older than
+    older_than = timedelta(seconds=4)  # time the notification can not be older than
     template = create_template(service=sample_service, template_type=notification_type)
     old_notification = create_notification(
-        template=template, created_at=datetime.utcnow() - timedelta(seconds=older_than), status="created"
+        template=template, created_at=datetime.utcnow() - older_than, status="created"
     )
-    create_notification(
-        template=template, created_at=datetime.utcnow() - timedelta(seconds=older_than), status="sending"
-    )
+    create_notification(template=template, created_at=datetime.utcnow() - older_than, status="sending")
     create_notification(template=template, created_at=datetime.utcnow(), status="created")
 
     results = notifications_not_yet_sent(older_than, notification_type, session=session)
@@ -1614,7 +1612,7 @@ def test_notifications_not_yet_sent(sample_service, notification_type, session):
 @pytest.mark.parametrize("session", (db.session, db.session_bulk), ids=("default", "bulk"))
 @pytest.mark.parametrize("notification_type", ["letter", "email", "sms"])
 def test_notifications_not_yet_sent_return_no_rows(sample_service, notification_type, session):
-    older_than = 5  # number of seconds the notification can not be older than
+    older_than = timedelta(seconds=4)  # time the notification can not be older than
     template = create_template(service=sample_service, template_type=notification_type)
     create_notification(template=template, created_at=datetime.utcnow(), status="created")
     create_notification(template=template, created_at=datetime.utcnow(), status="sending")
