ff36be4df64035fabf66fc82b309fbde27efdf9b
Ben Corlett <benjamin.corlett@digital.cabinet-office.gov.uk>
2026-02-10 07:31:38 +0000

    Increase worker_connections to 256 and enable keep alive

--- tests/test_gunicorn_config.py
+++ tests/test_gunicorn_config.py
@@ -4,5 +4,5 @@ from gunicorn_config import keepalive, timeout, worker_class, workers
 def test_gunicorn_config():
     assert workers == 4
     assert worker_class == "eventlet"
-    assert keepalive == 0
+    assert keepalive == 90
     assert timeout == 30
