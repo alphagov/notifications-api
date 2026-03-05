162f38f2439653e658dca627fc454478500d9143
Will Pearson <will.pearson@digital.cabinet-office.gov.uk>
2026-03-05 12:09:16 +0000

    Apply suggestions from code review
    Make it consitent with other descriptions
    
    Co-authored-by: spatel033 <177532680+spatel033@users.noreply.github.com>

--- app/authentication/auth.py
+++ app/authentication/auth.py
@@ -78,7 +78,7 @@ def requires_internal_auth(expected_client_id):
 
     _decode_jwt_token(auth_token, api_keys, client_id)
     g.service_id = client_id
-    # If other headers are required (or this one no longer need) update
+    # If other headers are required (or this one is no longer needed) update the docs:
     # https://github.com/alphagov/notifications-manuals/wiki/Request-headers-used
     g.user_id = request.headers.get("X-Notify-User-Id")
 
