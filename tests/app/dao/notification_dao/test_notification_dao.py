lakeraven/notifications-api-govuk

Adding get_notifcation_by_job_and_job_row_number and using it to filter exceptions
Job Id and job row number are unique in the notifications table so they can be used
to see if a notification has been added to the db already even if the notification
has been given a different notification id.

Handle error is the logic for all types of notification, but I only tested one save function
.

tests/app/dao/notification_dao/test_notification_dao.py
@@ -39,6 +39,7 @@ from app.dao.notifications_dao import (
     dao_update_notification,
     dao_update_notifications_by_reference,
     get_notification_by_id,
+    get_notification_by_job_and_job_row_number,
     get_notification_with_personalisation,
     get_notifications_for_job,
     get_notifications_for_service,
@@ -400,6 +401,20 @@ def test_get_notification_by_id_when_notification_exists(sample_notification):
 
 def test_get_notification_by_id_when_notification_does_not_exist(notify_db_session, fake_uuid):
     notification_from_db = get_notification_by_id(fake_uuid)
+
+    assert notification_from_db is None
+
+
+def test_get_notification_by_job_and_row_number_when_notification_exists(sample_notification):
+    notification_from_db = get_notification_by_job_and_job_row_number(
+        sample_notification.job_id, sample_notification.job_row_number
+    )
+
+    assert sample_notification == notification_from_db
+
+
+def test_get_notification_by_job_and_row_number_when_job_id_notification_does_not_exist(notify_db_session, fake_uuid):
+    notification_from_db = get_notification_by_job_and_job_row_number(fake_uuid, 1)
 
     assert notification_from_db is None
 

alphagov/notifications-api

tests/app/dao/notification_dao/test_notification_dao.py
@@ -39,6 +39,7 @@ from app.dao.notifications_dao import (
     dao_update_notification,
     dao_update_notifications_by_reference,
     get_notification_by_id,
+    get_notification_by_job_and_job_row_number,
     get_notification_with_personalisation,
     get_notifications_for_job,
     get_notifications_for_service,
@@ -401,6 +402,20 @@ def test_get_notification_by_id_when_notification_exists(sample_notification):
 
 def test_get_notification_by_id_when_notification_does_not_exist(notify_db_session, fake_uuid):
     notification_from_db = get_notification_by_id(fake_uuid)
+
+    assert notification_from_db is None
+
+
+def test_get_notification_by_job_and_row_number_when_notification_exists(sample_email_notification):
+    notification_from_db = get_notification_by_job_and_job_row_number(
+        sample_email_notification.job_id, sample_email_notification.job_row_number
+    )
+
+    assert sample_email_notification == notification_from_db
+
+
+def test_get_notification_by_job_and_row_number_when_job_id_notification_does_not_exist(notify_db_session, fake_uuid):
+    notification_from_db = get_notification_by_job_and_job_row_number(fake_uuid, 1)
 
     assert notification_from_db is None
 