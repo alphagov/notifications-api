2baa6dc7fe752ffee9670f9386802e434407c7d9
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2026-04-16 10:46:00 +0100

    ** DO NOT MERGE UNTIL https://github.com/alphagov/notifications-admin/pull/5858 is merged and deployed **
    remove "send_files_via_ui" service permission
    
    removes the "send_files_via_ui" service permission when the send files via email feature is released
    
    ** DO NOT MERGE UNTIL https://github.com/alphagov/notifications-admin/pull/5858 is merged and deployed **

--- app/constants.py
+++ app/constants.py
@@ -234,7 +234,6 @@ EMAIL_AUTH = "email_auth"
 EDIT_FOLDER_PERMISSIONS = "edit_folder_permissions"
 INTERNATIONAL_LETTERS = "international_letters"
 SMS_TO_UK_LANDLINES = "sms_to_uk_landlines"
-SEND_FILES_VIA_UI = "send_files_via_ui"
 SERVICE_PERMISSION_TYPES = [
     EMAIL_TYPE,
     SMS_TYPE,
@@ -245,7 +244,6 @@ SERVICE_PERMISSION_TYPES = [
     EDIT_FOLDER_PERMISSIONS,
     INTERNATIONAL_LETTERS,
     SMS_TO_UK_LANDLINES,
-    SEND_FILES_VIA_UI,
 ]
 
 # List of available permissions
