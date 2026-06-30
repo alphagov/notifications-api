41f62c0fb63580bb56d81550a15749bda2d953be
Chukwugozie Mbeledogu <chukwugozie.mbeledogu@digital.cabinet-office.gov.uk>
2026-06-30 10:19:32 +0100

    Add v2 get_returned_letters endpoint  This endpoint returns the same returned letters report displayed in a service's dashboard  for a given report date.

--- app/v2/returned_letters/get_returned_letters.py
+++ app/v2/returned_letters/get_returned_letters.py
@@ -1,16 +1,13 @@
-from datetime import UTC, datetime, timedelta
-from typing import Any
-from uuid import UUID
+from flask import jsonify, request
 
-from flask import abort, jsonify, make_response, request
-from notifications_utils.clients.redis import RequestCache
-
-from app import authenticated_service, redis_store, api_user
-from app.dao.returned_letters_dao import fetch_returned_letter_summary
+from app import api_user, authenticated_service
+from app.dao.returned_letters_dao import count_orphaned_returned_letters, fetch_returned_letter_summary
 from app.errors import InvalidRequest
 from app.schema_validation import validate
+from app.service.rest import _fetch_returned_letter_data
 from app.utils import DATE_FORMAT
 from app.v2.returned_letters import v2_returned_letters_blueprint
+from app.v2.returned_letters.get_returned_letters_schema import get_returned_letters_request
 
 
 @v2_returned_letters_blueprint.route("/summary", methods=["GET"])
@@ -34,3 +31,35 @@ def get_returned_letter_summary():
     return jsonify(data), 200
 
 
+@v2_returned_letters_blueprint.route("/", methods=["GET"])
+def get_returned_letters():
+    """
+    This endpoint returns the latest returned letters for a service
+    """
+    if api_user.key_type != "normal":
+        raise InvalidRequest("Only live API keys are authorised to get the latest returned letter summary.", 403)
+
+    service_id = str(authenticated_service.id)
+    data = validate(request.args.to_dict(), get_returned_letters_request)
+    report_date = data.get("report_date")
+    result = [
+        {
+            "notification_id": str(row["notification_id"]),
+            "reference": row["client_reference"],
+            "report_date": row["reported_at"],
+            "created_at": row["created_at"],
+            "email_address": row["email_address"],
+            "template_name": str(row["template_name"]),
+            "template_id": str(row["template_id"]),
+            "template_version": row["template_version"],
+            "spreadsheet_file_name": row["original_file_name"],
+            "spreadsheet_row_number": row["job_row_number"],
+            "uploaded_letter_file_name": row["uploaded_letter_file_name"],
+        }
+        for row in _fetch_returned_letter_data(service_id, report_date)
+    ]
+
+    return {
+        "returned_letters": sorted(result, key=lambda i: i["created_at"], reverse=True),
+        "orphaned_count": count_orphaned_returned_letters(service_id, report_date),
+    }
--- tests/app/v2/returned_letters/test_get_returned_letters.py
+++ tests/app/v2/returned_letters/test_get_returned_letters.py
@@ -2,7 +2,6 @@ import uuid
 from datetime import UTC, datetime, timedelta
 
 from freezegun import freeze_time
-from notifications_utils.testing.comparisons import AnyStringMatching, RestrictedAny
 
 from app.constants import NOTIFICATION_RETURNED_LETTER
 from app.utils import DATETIME_FORMAT_NO_TIMEZONE
@@ -37,10 +36,207 @@ def test_v2_get_returned_letter_summary(api_client_request, sample_service):
     ]
 
 
-def test_v2_get_returned_letter_summary_error_for_non_live_keys(api_client_request, sample_service):
+def test_v2_get_returned_letter_summary_raises_error_for_non_live_keys(api_client_request, sample_service):
     api_client_request.get(
         sample_service.id,
         "v2_returned_letters.get_returned_letter_summary",
-        _api_key_type='test',
+        _api_key_type="test",
+        _expected_status=403,
+    )
+
+
+@freeze_time("2026-06-23 13:30")
+def test_v2_get_returned_endpoint(api_client_request, sample_letter_template):
+    job = create_job(template=sample_letter_template)
+    letter_from_job = create_notification(
+        template=sample_letter_template,
+        client_reference="letter_from_job",
+        status=NOTIFICATION_RETURNED_LETTER,
+        job=job,
+        job_row_number=2,
+        created_at=datetime.now(UTC) - timedelta(days=1),
+        created_by_id=sample_letter_template.service.users[0].id,
+    )
+    create_returned_letter(
+        service=sample_letter_template.service, reported_at=datetime.now(UTC), notification_id=letter_from_job.id
+    )
+
+    one_off_letter = create_notification(
+        template=sample_letter_template,
+        status=NOTIFICATION_RETURNED_LETTER,
+        created_at=datetime.now(UTC) - timedelta(days=2),
+        created_by_id=sample_letter_template.service.users[0].id,
+    )
+    create_returned_letter(
+        service=sample_letter_template.service, reported_at=datetime.utcnow(), notification_id=one_off_letter.id
+    )
+
+    api_key = create_api_key(service=sample_letter_template.service)
+    api_letter = create_notification(
+        template=sample_letter_template,
+        client_reference="api_letter",
+        status=NOTIFICATION_RETURNED_LETTER,
+        created_at=datetime.now(UTC) - timedelta(days=3),
+        api_key=api_key,
+    )
+    create_returned_letter(
+        service=sample_letter_template.service, reported_at=datetime.utcnow(), notification_id=api_letter.id
+    )
+
+    precompiled_template = create_template(
+        service=sample_letter_template.service, template_type="letter", hidden=True, template_name="hidden template"
+    )
+    precompiled_letter = create_notification_history(
+        template=precompiled_template,
+        api_key=api_key,
+        client_reference="precompiled letter",
+        created_at=datetime.now(UTC) - timedelta(days=4),
+    )
+    create_returned_letter(
+        service=sample_letter_template.service, reported_at=datetime.utcnow(), notification_id=precompiled_letter.id
+    )
+
+    uploaded_letter = create_notification_history(
+        template=precompiled_template,
+        client_reference="filename.pdf",
+        created_at=datetime.now(UTC) - timedelta(days=5),
+        created_by_id=sample_letter_template.service.users[0].id,
+    )
+    create_returned_letter(
+        service=sample_letter_template.service, reported_at=datetime.now(UTC), notification_id=uploaded_letter.id
+    )
+
+    # not included in results because wrong service
+    not_included_in_results_template = create_template(
+        service=create_service(service_name="not included in results"), template_type="letter"
+    )
+    letter_4 = create_notification_history(
+        template=not_included_in_results_template, status=NOTIFICATION_RETURNED_LETTER
+    )
+    create_returned_letter(
+        service=not_included_in_results_template.service, reported_at=datetime.now(UTC), notification_id=letter_4.id
+    )
+
+    # not included in results because wrong reported_at
+    letter_5 = create_notification_history(
+        template=precompiled_template,
+        client_reference="filename.pdf",
+        created_at=datetime.now(UTC) - timedelta(days=6),
+        created_by_id=sample_letter_template.service.users[0].id,
+    )
+    create_returned_letter(
+        service=sample_letter_template.service,
+        reported_at=datetime.now(UTC) - timedelta(days=2),
+        notification_id=letter_5.id,
+    )
+
+    # two "orphaned" letters
+    create_returned_letter(
+        service=sample_letter_template.service,
+        reported_at=datetime.now(UTC),
+        notification_id=uuid.uuid4(),
+    )
+    create_returned_letter(
+        service=sample_letter_template.service,
+        reported_at=datetime.now(UTC),
+        notification_id=uuid.uuid4(),
+    )
+
+    # an "orphaned" letter for the wrong day
+    create_returned_letter(
+        service=sample_letter_template.service,
+        reported_at=datetime.now(UTC) - timedelta(days=3),
+        notification_id=uuid.uuid4(),
+    )
+
+    # an "orphaned" letter for the wrong service
+    create_returned_letter(
+        service=not_included_in_results_template.service,
+        reported_at=datetime.now(UTC),
+        notification_id=uuid.uuid4(),
+    )
+
+    response = api_client_request.get(
+        sample_letter_template.service.id, "v2_returned_letters.get_returned_letters", report_date="2026-06-23"
+    )
+
+    assert response == {
+        "returned_letters": [
+            {
+                "notification_id": str(letter_from_job.id),
+                "reference": None,
+                "report_date": "2026-06-23",
+                "created_at": letter_from_job.created_at.strftime(DATETIME_FORMAT_NO_TIMEZONE),
+                "email_address": letter_from_job.get_created_by_email_address(),
+                "template_name": sample_letter_template.name,
+                "template_id": str(sample_letter_template.id),
+                "template_version": sample_letter_template.version,
+                "spreadsheet_file_name": job.original_file_name,
+                "spreadsheet_row_number": 4,
+                "uploaded_letter_file_name": None,
+            },
+            {
+                "notification_id": str(one_off_letter.id),
+                "reference": None,
+                "report_date": "2026-06-23",
+                "created_at": one_off_letter.created_at.strftime(DATETIME_FORMAT_NO_TIMEZONE),
+                "email_address": one_off_letter.get_created_by_email_address(),
+                "template_name": sample_letter_template.name,
+                "template_id": str(sample_letter_template.id),
+                "template_version": sample_letter_template.version,
+                "spreadsheet_file_name": None,
+                "spreadsheet_row_number": None,
+                "uploaded_letter_file_name": None,
+            },
+            {
+                "notification_id": str(api_letter.id),
+                "reference": api_letter.client_reference,
+                "report_date": "2026-06-23",
+                "created_at": api_letter.created_at.strftime(DATETIME_FORMAT_NO_TIMEZONE),
+                "email_address": "API",
+                "template_name": sample_letter_template.name,
+                "template_id": str(sample_letter_template.id),
+                "template_version": sample_letter_template.version,
+                "spreadsheet_file_name": None,
+                "spreadsheet_row_number": api_letter.job_row_number,
+                "uploaded_letter_file_name": None,
+            },
+            {
+                "notification_id": str(precompiled_letter.id),
+                "reference": precompiled_letter.client_reference,
+                "report_date": "2026-06-23",
+                "created_at": precompiled_letter.created_at.strftime(DATETIME_FORMAT_NO_TIMEZONE),
+                "email_address": "API",
+                "template_name": "None",
+                "template_id": "None",
+                "template_version": None,
+                "spreadsheet_file_name": None,
+                "spreadsheet_row_number": precompiled_letter.job_row_number,
+                "uploaded_letter_file_name": None,
+            },
+            {
+                "notification_id": str(uploaded_letter.id),
+                "reference": None,
+                "report_date": "2026-06-23",
+                "created_at": uploaded_letter.created_at.strftime(DATETIME_FORMAT_NO_TIMEZONE),
+                "email_address": sample_letter_template.service.users[0].email_address,
+                "template_name": "None",
+                "template_id": "None",
+                "template_version": None,
+                "spreadsheet_file_name": None,
+                "spreadsheet_row_number": uploaded_letter.job_row_number,
+                "uploaded_letter_file_name": "filename.pdf",
+            },
+        ],
+        "orphaned_count": 2,
+    }
+
+
+def test_v2_get_returned_letters_raises_error_for_non_live_keys(api_client_request, sample_service):
+    api_client_request.get(
+        sample_service.id,
+        "v2_returned_letters.get_returned_letters",
+        report_date="2026-06-23",
+        _api_key_type="test",
         _expected_status=403,
     )
