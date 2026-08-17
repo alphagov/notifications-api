9c6385642cc6e678f083c2808fe1ede74b9f2084
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2026-08-17 10:08:40 +0100

    Add migration to enable replica identity using index on notifications
    This will allow us to listen to changes made to the date and status

---
+++ migrations/versions/0563_enable_replica_id_index.py
@@ -0,0 +1,15 @@
+"""
+Create Date: 2026-08-17 00:00:00
+"""
+
+from alembic import op
+
+revision = "0563_enable_replica_id_index"
+down_revision = "0562_create_replacation_slot"
+
+
+def upgrade():
+    op.execute("ALTER TABLE notifications REPLICA IDENTITY USING INDEX ix_notifications_id_status;")
+
+def downgrade():
+    op.execute("ALTER TABLE notifications REPLICA IDENTITY DEFAULT;")
