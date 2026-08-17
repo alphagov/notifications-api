a10271c00f0dafcad8de045f48fa29ccd2a9e22c
Chris Hill-Scott <me@quis.cc>
2026-08-17 11:16:11 +0100

    Send non-GSM-7 characters in text messages

--- app/delivery/send_to_providers.py
+++ app/delivery/send_to_providers.py
@@ -60,9 +60,9 @@ def send_sms_to_provider(notification: Notification) -> None:
             show_prefix=service.prefix_sms,
         )
 
-        if non_gsm_characters := SanitiseSMS.get_non_gsm_characters(template.unsanitised_content):
+        if non_gsm_characters := template.non_gsm_characters:
             current_app.logger.warning(
-                "%s character(s) replaced with ? in SMS content for service %s and notification %s",
+                "%s character(s) caused UTF-16 encoding in SMS content for service %s and notification %s",
                 len(non_gsm_characters),
                 service.id,
                 notification.id,
