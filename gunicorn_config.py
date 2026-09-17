c084271586bf057d5102f71274c6f9021854bbf0
James Robinson <james.robinson2@dsit.gov.uk>
2026-09-17 14:23:06 +0100

    gunicorn_config: Enable OpenTelemetry instrumentation
    OtelAwareEventletWorker automatically runs the OTel auto-instrumentation
    at the appropriate point in the worker lifecycle.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,7 +6,7 @@ set_gunicorn_defaults(globals())
 
 
 workers = int(os.getenv("GUNICORN_WORKERS", "4"))
-worker_class = "eventlet"
+worker_class = "notifications_utils.gunicorn.eventlet.OtelAwareEventletWorker"
 worker_connections = int(os.getenv("GUNICORN_WORKER_CONNECTIONS", "8"))
 keepalive = int(os.getenv("GUNICORN_KEEPALIVE", "0"))
 timeout = int(os.getenv("HTTP_SERVE_TIMEOUT_SECONDS", 30))  # though has little effect with eventlet worker_class
